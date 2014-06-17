5f323e83e211338baea2e96cc146dd502c83ca0c
Camilo Carrillo <ccarrillom@gmail.com>
2014-06-17 17:17:10 +0200

    Cloning ZeroVertexSelector for Booking space

--- MicroAODAlgos/plugins/LegacyVertexSelector.cc
+++ MicroAODAlgos/plugins/LegacyVertexSelector.cc
@@ -6,25 +6,25 @@
 
 namespace flashgg {
   
-  class ZerothVertexSelector : public VertexSelectorBase {
+  class LegacyVertexSelector : public VertexSelectorBase {
   public:
-    ZerothVertexSelector(const edm::ParameterSet& conf) :
+    LegacyVertexSelector(const edm::ParameterSet& conf) :
       VertexSelectorBase(conf),
       _whichVertex(conf.getUntrackedParameter<unsigned int>("whichVertex",0)) {}
 
     edm::Ptr<reco::Vertex> select(const edm::Ptr<flashgg::Photon>&,const edm::Ptr<flashgg::Photon>&,const edm::PtrVector<reco::Vertex>&) const override;
 
   private:
-    unsigned int _whichVertex; // set this variable to something non-zero to make this stupid selector both stupider and poorly-named
-                               // Also it would cause crashes if there was only 1 vertex in the collection
+    unsigned int _whichVertex; 
   };
 
-  edm::Ptr<reco::Vertex> ZerothVertexSelector::select(const edm::Ptr<flashgg::Photon>& g1,const edm::Ptr<flashgg::Photon>& g2,const edm::PtrVector<reco::Vertex>& vtxs) const {
+  edm::Ptr<reco::Vertex> LegacyVertexSelector::select(const edm::Ptr<flashgg::Photon>& g1,const edm::Ptr<flashgg::Photon>& g2,const edm::PtrVector<reco::Vertex>& vtxs) const {
+    std::cout<<"Running the LegacyVertexSelector"<<std::endl;
     return vtxs[_whichVertex];
   }
 
 }
 
 DEFINE_EDM_PLUGIN(FlashggVertexSelectorFactory,
-		  flashgg::ZerothVertexSelector,
-		  "FlashggZerothVertexSelector");
+		  flashgg::LegacyVertexSelector,
+		  "FlashggLegacyVertexSelector");
