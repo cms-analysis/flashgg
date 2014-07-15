800e0722e1fbdf14f5c223a1bd3aad162d238fdc
Manuel <manuel.alejandro.olmedo.negrete@cern.ch>
2014-07-15 13:34:11 +0200

    some finalizing updates to scan_test_output_photon.C

--- MicroAODProducers/test/scan_test_output_photon.C
+++ MicroAODProducers/test/scan_test_output_photon.C
@@ -1,5 +1,5 @@
 {
-  // Set up FW Lite for automatic loading of CMS libraries
+// Set up FW Lite for automatic loading of CMS libraries
   // and data formats.   As you may have other user-defined setup
   // in your rootlogon.C, the CMS setup is executed only if the CMS
   // environment is set up.
@@ -16,26 +16,29 @@
     gSystem->Load("libDataFormatsFWLite.so");
     gSystem->Load("libDataFormatsPatCandidates.so");
     gSystem->Load("libflashggMicroAODFormats.so"); 
-}
 
+}
+  
   TFile f("myOutputFile.root");
   TTree *Events = f.Get("Events");
   Events->Print();
   Events->SetScanField(0);
-  Events->Scan("flashggDiPhotonCandidates_flashggDiPhotons__FLASHggTEST.obj.pt_:flashggPhotons_flashggPhotons__FLASHggTEST.obj.pt_:flashggPhotons_flashggPhotons__FLASHggTEST.obj.full5x5_showerShape_.sigmaIetaIeta:recoVertexs_offlineSlimmedPrimaryVertices__PAT.obj.chi2_");
+  Events->Scan("flashggDiPhotonCandidates_flashggDiPhotons__FLASHggTEST.obj.pt_:flashggPhotons_flashggPhotons__FLASHggTEST.obj.zernike42:flashggPhotons_flashggPhotons__FLASHggTEST.obj.full5x5_showerShape_.hcalDepth1OverEcalBc");
+
+
 #include "DataFormats/FWLite/interface/Event.h"
 #include "DataFormats/FWLite/interface/Handle.h"
-#include <typeinfo>
 #if !defined(__CINT__) && !defined(__MAKECINT__)
 #include "flashgg/MicroAODFormats/interface/DiphotonCandidate.h"  
 #include "flashgg/MicroAODFormats/interface/Photon.h"  
 #endif
 
+  TH1F*pho_hist0 = new TH1F("phoPt","phoPt",200,0,500); 
+
   fwlite::Event ev(&f);
 
-  int n = 0;
 
-for( ev.toBegin(); ! ev.atEnd(); ++ev) {
+    for( ev.toBegin(); ! ev.atEnd(); ++ev) {
     fwlite::Handle<std::vector<flashgg::Photon> > objs_pho;
     fwlite::Handle<std::vector<flashgg::DiPhotonCandidate> > objs_dipho;
     fwlite::Handle<std::vector<reco::Vertex> > objs_vertex;
@@ -43,10 +46,8 @@ for( ev.toBegin(); ! ev.atEnd(); ++ev) {
     objs_pho.getByLabel(ev,"flashggPhotons");
     objs_dipho.getByLabel(ev,"flashggDiPhotons");
     objs_vertex.getByLabel(ev,"offlineSlimmedPrimaryVertices"); 
-    objs_packcand.getByLabel(ev,"flashggVertexMapNonUnique"); 
 
-    std::cout << "-----------------------" << std::endl;
-   
+std::cout  << "------------------------------------------------------------------------" << std::endl;   
     // now can access data
     for (int i=0; i < objs_pho.ptr()->size();i++ ){
     std::vector<flashgg::Photon> const & pho = *objs_pho;
@@ -57,28 +58,48 @@ for( ev.toBegin(); ! ev.atEnd(); ++ev) {
     std::cout << "e2x5" << "   " << pho[i].e2x5() << std::endl;
     std::cout << "e5x5" << "   " << pho[i].e5x5() << std::endl;
     std::cout << "e3x3" << "   " << pho[i].e3x3() << std::endl;
+//    std::cout << "e1x3" << "   " << pho[i].e1x3() << std::endl;
+//    std::cout << "ebottom" << "   " << pho[i].ebottom() << std::endl;
+//    std::cout << "etop" << "   " << pho[i].etop() << std::endl;
+//    std::cout << "eright" << "   " << pho[i].eright() << std::endl;
+//    std::cout << "e2x5bottom" << "   " << pho[i].e2x5bottom() << std::endl;
+//    std::cout << "e2x5top" << "   " << pho[i].el2x5top() << std::endl;
+//    std::cout << "e2x5left" << "   " << pho[i].e2x5left() << std::endl;
+//    std::cout << "e2x5right" << "   " << pho[i].eright() << std::endl;
+//    std::cout << "e2x5max" << "   " << pho[i].emax() << std::endl;
+//    std::cout << "e2nd" << "   " << pho[i].e2nd() << std::endl;
+//    std::cout << "zernike42" << "   " << pho[i].zernike42() << std::endl;
+//    std::cout << "zernike20" << "   " << pho[i].zernike20() << std::endl;
+//    std::cout << "sieip" << "   " << pho[i].sieip() << std::endl;
+//    std::cout << "sipip" << "   " << pho[i].sipip() << std::endl;
+//    std::cout << "hcalDepth2OverEcalBc" << "   " << pho[i].hcalDepth2OverEcalBc() << std::endl;
+//    std::cout << "hcalDepth1OverEcalBc" << "   " << pho[i].hcalDepth1OverEcalBc() << std::endl;
+//    std::cout << "hcalDepth2OverEcal" << "   " << pho[i].hcalDepth2OverEcal() << std::endl;
+//    std::cout << "hcalDepth1OverEcal" << "   " << pho[i].hcalDepth1OverEcal() << std::endl;
     std::cout << "sigmaIetaIeta" << "   " << pho[i].sigmaIetaIeta() << std::endl;
     std::cout << "sigmaEtaEta"   << "   " << pho[i].sigmaEtaEta() << std::endl;
     std::cout << "maxEnergyXtal" << "   " << pho[i].maxEnergyXtal() << std::endl;
+
+     pho_hist0->Fill(pho[i].pt());
        }
+
     for (int i=0; i < objs_dipho.ptr()->size();i++ ){
     std::vector<flashgg::DiPhotonCandidate> const & dipho = *objs_dipho;
     std::cout << "pt dipho" <<   "   " << dipho[i].pt() << std::endl;
     std::cout << "eta dipho" <<  "   " << dipho[i].eta() << std::endl;
     std::cout << "phi dipho" <<  "   " << dipho[i].phi() << std::endl;
-    }
-    for (int i=0; i < objs_vertex.ptr()->size();i++ ){
-    std::vector<reco::Vertex> const & vertex = *objs_vertex;
-    std::cout << "chi2" <<   "   " << vertex[i].chi2() << std::endl;
-     } 
+       }
 
+//    for (int i=0; i < objs_vertex.ptr()->size();i++ ){
+//    std::vector<reco::Vertex> const & vertex = *objs_vertex;
+//    std::cout << "chi2" <<   "   " << vertex[i].chi2() << std::endl;
+//     } 
 
-
-
-
-
-
-    }
+     }
+TCanvas * c3 = new TCanvas();
+pho_hist0->Draw();     
+c3->SaveAs("pho_hist0.pdf");
 
 }
+
 
