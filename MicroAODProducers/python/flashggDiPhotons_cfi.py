b35db7f156adc9e61d17ab645dd60925f131aed4
Seth Zenz <seth.zenz@cern.ch>
2014-07-19 09:09:58 +0200

    assorted cleanup: change folder name only to comissioning, fix simple_Producer_test.py, put BDT weights into MicroAODProducers/data, remove scram-produced __init__.py files

--- MicroAODProducers/python/flashggDiPhotons_cfi.py
+++ MicroAODProducers/python/flashggDiPhotons_cfi.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 flashggDiPhotons = cms.EDProducer('FlashggDiPhotonProducer',
                                   PhotonTag=cms.untracked.InputTag('flashggPhotons'),
                                   VertexTag=cms.untracked.InputTag('offlineSlimmedPrimaryVertices'),
-#                                         VertexSelectorName=cms.string("FlashggZerothVertexSelector"),
                                   VertexSelectorName=cms.string("FlashggLegacyVertexSelector"),
                                   VertexCandidateMapTag=cms.InputTag("flashggVertexMapUnique")
                                   )
--- MicroAODProducers/test/simple_Producer_test.py
+++ MicroAODProducers/test/simple_Producer_test.py
@@ -4,23 +4,10 @@ import FWCore.Utilities.FileUtils as FileUtils
 process = cms.Process("FLASHggTEST")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-#process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
-#process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
-#process.load("Geometry.CaloEventSetup.CaloGeometry_cfi")
-#process.load("CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi")
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-
-
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'POSTLS170_V5::All'
-#process.GlobalTag.toGet = cms.VPSet(
-#    cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
-#             tag = cms.string("EcalIntercalibConstants_Bon_V20101105"),
-#             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_ECAL")
-#             )
-#    )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
@@ -41,24 +28,8 @@ process.flashggVertexMapNonUnique = cms.EDProducer('FlashggDzVertexMapProducer',
                                                    UseEachTrackOnce=cms.untracked.bool(False)
                                                    )
 
-process.flashggPhotons = cms.EDProducer('FlashggPhotonProducer',
-                                        PhotonTag=cms.untracked.InputTag('slimmedPhotons'),
-                                        PFCandidatesTag=cms.untracked.InputTag('packedPFCandidates'), # Needed to compute ChargedPFIso for Run1 Legacy preselection
-                                        PhotonPreselectorName=cms.string("FlashggNoPhotonPreselector"),
-#                                        PhotonPreselectorName=cms.string("FlashggLegacyPhotonPreselector"),
-                                        reducedBarrelRecHitCollection=cms.InputTag('reducedEgamma','reducedEBRecHits'),
-                                        reducedEndcapRecHitCollection=cms.InputTag('reducedEgamma','reducedEERecHits'),
-                                        reducedPreshowerRecHitCollection=cms.InputTag('reducedEgamma','reducedESRecHits')
-                                        )
-
-process.flashggDiPhotons = cms.EDProducer('FlashggDiPhotonProducer',
-                                          PhotonTag=cms.untracked.InputTag('flashggPhotons'),
-                                          VertexTag=cms.untracked.InputTag('offlineSlimmedPrimaryVertices'),
-#                                         VertexSelectorName=cms.string("FlashggZerothVertexSelector"),
-                                          VertexSelectorName=cms.string("FlashggLegacyVertexSelector"),
-                                          VertexCandidateMapTag=cms.InputTag("flashggVertexMapUnique")  
-#                                          VertexCandidateMapTag=cms.InputTag("flashggVertexMapNonUnique")
-                                          )
+process.load("flashgg/MicroAODProducers/flashggPhotons_cfi")
+process.load("flashgg/MicroAODProducers/flashggDiPhotons_cfi")
 
 process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string('myOutputFile.root'),
                                outputCommands = cms.untracked.vstring("drop *",
