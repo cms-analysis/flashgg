b07ca5011451e2fb388acf365b040e9e90e6013f
Manuel <manuel.alejandro.olmedo.negrete@cern.ch>
2014-07-22 18:04:53 +0200

    first try at calculating vtx variables sumpt2, ptasym, ptbal

--- MicroAODAlgos/plugins/LegacyVertexSelector.cc
+++ MicroAODAlgos/plugins/LegacyVertexSelector.cc
@@ -3,6 +3,8 @@
 #include "flashgg/MicroAODFormats/interface/Photon.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/Common/interface/PtrVector.h"
+#include "TVector3.h"
+#include "TVector2.h"
 
 namespace flashgg {
   
@@ -13,27 +15,53 @@ namespace flashgg {
 
     edm::Ptr<reco::Vertex> select(const edm::Ptr<flashgg::Photon>&,const edm::Ptr<flashgg::Photon>&,const edm::PtrVector<reco::Vertex>&,
                                   const VertexCandidateMap& vertexCandidateMap) const override;
-
+   
   private:
-    unsigned int _whichVertex; 
-  };
+    unsigned int _whichVertex;
+     };
+   
+
+    TVector3 diPho;  
+    TVector3 tk;
+    TVector2 tkPlane;
+    TVector2 diPhoPlane;
+    double sumpt = 0;
+    double sumpt2 = 0;  
+    double ptbal = 0; 
+    double ptasym = 0;
 
   edm::Ptr<reco::Vertex> LegacyVertexSelector::select(const edm::Ptr<flashgg::Photon>& g1,const edm::Ptr<flashgg::Photon>& g2,const edm::PtrVector<reco::Vertex>& vtxs,
 						      const VertexCandidateMap& vertexCandidateMap) const {
     std::cout<<"Running the LegacyVertexSelector"<<std::endl;
 
     // Just an example for how to read the vertex-candidate association -- doesn't do anything (yet)
-    bool verbose = false;
-    if (verbose) {
+    //bool verbose = false;
+    //if (verbose) {
+       diPho.SetXYZ(g1->px()+g2->px(),g1->py()+g2->py(),g1->pz()+g2->pz());
+       diPhoPlane = diPho.XYvector();
+        
       for (unsigned int i = 0 ; i < vtxs.size() ; i++) {
 	edm::Ptr<reco::Vertex> vtx = vtxs[i];
 	std::cout << " On vertex " << i << " with z position " << vtx->position().z() << std::endl;
 	for (unsigned int j = 0 ; j < vertexCandidateMap.at(vtx).size() ; j++) {
-	  edm::Ptr<pat::PackedCandidate> cand = vertexCandidateMap.at(vtx)[j];
-	  std::cout << " Candidate " << j << " in vertex " << i << " has dz (w.r.t that vertex) of  " << cand->dz(vtx->position()) << std::endl;
+	    edm::Ptr<pat::PackedCandidate> cand = vertexCandidateMap.at(vtx)[j];
+            tk.SetXYZ(cand->px(),cand->py(),cand->pz());  
+            tkPlane = tk.XYvector();
+            sumpt += tkPlane.Mod(); 
+            sumpt2 += tkPlane.Mod2();
+            ptbal -= tkPlane * diPhoPlane.Unit(); 	
+            
+	 //std::cout << " Candidate " << j << " in vertex " << i << " has dz (w.r.t that vertex) of  " << cand->dz(vtx->position()) << std::endl;
 	}
+         ptasym = (sumpt - diPhoPlane.Mod())/(sumpt+diPhoPlane.Mod());
+
+         std::cout << "sumpt2" << "  " << sumpt2  << std::endl;
+         std::cout << "ptbal" << "  " << ptbal   << std::endl;
+         std::cout << "ptasym" << "  " << ptasym  << std::endl;
+
+
       }
-    }
+    //}
 
     return vtxs[0];
   }
