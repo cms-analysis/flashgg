51df00ff621039f24f651e9a727aa127dd53f612
Olivier <olivier.bondu@gmail.com>
2014-07-28 16:52:54 +0200

    Fixes to get compliant with details from https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideCreatingNewProducts

--- MicroAODFormats/src/classes.h
+++ MicroAODFormats/src/classes.h
@@ -5,15 +5,24 @@
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include <vector>
 namespace  { struct dictionary {
-  std::vector<flashgg::Photon> dummy0;
-  edm::Wrapper<std::vector<flashgg::Photon> > dummy1;
-  std::vector<flashgg::DiPhotonCandidate> dummy2;
-  edm::Wrapper<std::vector<flashgg::DiPhotonCandidate> > dummy3;
-  edm::Ptr<reco::Vertex> dummy4;
-  std::vector<flashgg::Jet> dummy5;
-  edm::Wrapper<std::vector<flashgg::Jet> > dummy6;
-  edm::PtrVector<pat::PackedCandidate> dummy7;
-  std::map<edm::Ptr<reco::Vertex>,edm::PtrVector<pat::PackedCandidate> > dummy8;
-  edm::Wrapper<std::map<edm::Ptr<reco::Vertex>,edm::PtrVector<pat::PackedCandidate> > > dummy9;
-  std::map<edm::Ptr<reco::Vertex>,float> dummy10;
+  flashgg::Photon                                                   fgg_pho;
+  edm::Wrapper<flashgg::Photon>                                 wrp_fgg_pho;
+  std::vector<flashgg::Photon>                                  vec_fgg_pho;
+  edm::Wrapper<std::vector<flashgg::Photon> >               wrp_vec_fgg_pho;
+  flashgg::DiPhotonCandidate                                        fgg_dip;
+  edm::Wrapper<flashgg::DiPhotonCandidate>                      wrp_fgg_dip;
+  std::vector<flashgg::DiPhotonCandidate>                       vec_fgg_dip;
+  edm::Wrapper<std::vector<flashgg::DiPhotonCandidate> >    wrp_vec_fgg_dip;
+  edm::Ptr<reco::Vertex>                                        ptr_rec_vtx;
+  flashgg::Jet                                                      fgg_jet;
+  edm::Wrapper<flashgg::Jet>                                    wrp_fgg_jet;
+  std::vector<flashgg::Jet>                                     vec_fgg_jet;
+  edm::Wrapper<std::vector<flashgg::Jet> >                  wrp_vec_fgg_jet;
+  edm::PtrVector<pat::PackedCandidate>                          ptr_vec_pac;
+  std::map<edm::Ptr<reco::Vertex>,edm::PtrVector<pat::PackedCandidate> >    map_ptr_vtx_ptr_vec_pac;
+  std::pair<edm::Ptr<reco::Vertex>,edm::PtrVector<pat::PackedCandidate> >   pai_ptr_vtx_ptr_vec_pac;
+  edm::Wrapper<std::map<edm::Ptr<reco::Vertex>,edm::PtrVector<pat::PackedCandidate> > >     wrp_map_ptr_vtx_ptr_vec_pac;
+  edm::Wrapper<std::pair<edm::Ptr<reco::Vertex>,edm::PtrVector<pat::PackedCandidate> > >    wrp_pai_ptr_vtx_ptr_vec_pac;
+  std::map<edm::Ptr<reco::Vertex>,float>                    map_ptr_vtx_flo;
+  std::pair<edm::Ptr<reco::Vertex>,float>                   pai_ptr_vtx_flo;
 };}
