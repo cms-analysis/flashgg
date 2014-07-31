d6c70763084b639be8f5e0ee42edfb7fd9501a0d
Olivier <olivier.bondu@gmail.com>
2014-07-31 20:17:48 +0200

    Adding Charged PF iso cut that was missing out of the preselection

--- MicroAODProducers/python/flashggPreselectedDiPhotons_cfi.py
+++ MicroAODProducers/python/flashggPreselectedDiPhotons_cfi.py
@@ -11,13 +11,14 @@ flashggPreselectedDiPhotons = cms.EDFilter("CandViewSelector",
                                     || (leadingPhoton.isEE && leadingPhoton.hadronicOverEm < 0.075 && leadingPhoton.sigmaIetaIeta < 0.034)) \
                                     && (leadingPhoton.hcalTowerSumEtConeDR03 - 0.005 * leadingPhoton.pt < 4.0) \
                                     && (leadingPhoton.trkSumPtHollowConeDR03 - 0.002 * leadingPhoton.pt < 4.0) \
-                                    && (leadingPhoton.getpfPhoIso03 < 4.0) \
+                                    && (leadingPhoton.getpfChgIsoWrtChosenVtx02 < 4.0) \
                                     )) \
                                || (leadingPhoton.r9 > 0.9 \
                                     && ( ((leadingPhoton.isEB && leadingPhoton.hadronicOverEm < 0.082 && leadingPhoton.sigmaIetaIeta < 0.014) \
                                     || (leadingPhoton.isEE && leadingPhoton.hadronicOverEm < 0.075 && leadingPhoton.sigmaIetaIeta < 0.034)) \
                                     && (leadingPhoton.hcalTowerSumEtConeDR03 - 0.005 * leadingPhoton.pt < 50.0) \
                                     && (leadingPhoton.trkSumPtHollowConeDR03 - 0.002 * leadingPhoton.pt < 50.0) \
+                                    && (leadingPhoton.getpfChgIsoWrtChosenVtx02 < 4.0) \
                                     )) \
                              ) && ( \
                               (subLeadingPhoton.r9 <= 0.9 \
@@ -25,15 +26,16 @@ flashggPreselectedDiPhotons = cms.EDFilter("CandViewSelector",
                                     || (subLeadingPhoton.isEE && subLeadingPhoton.hadronicOverEm < 0.075 && subLeadingPhoton.sigmaIetaIeta < 0.034)) \
                                     && (subLeadingPhoton.hcalTowerSumEtConeDR03 - 0.005 * subLeadingPhoton.pt < 4.0) \
                                     && (subLeadingPhoton.trkSumPtHollowConeDR03 - 0.002 * subLeadingPhoton.pt < 4.0) \
+                                    && (subLeadingPhoton.getpfChgIsoWrtChosenVtx02 < 4.0) \
                                     )) \
                                || (subLeadingPhoton.r9 > 0.9 \
                                     && ( ((subLeadingPhoton.isEB && subLeadingPhoton.hadronicOverEm < 0.082 && subLeadingPhoton.sigmaIetaIeta < 0.014) \
                                     || (subLeadingPhoton.isEE && subLeadingPhoton.hadronicOverEm < 0.075 && subLeadingPhoton.sigmaIetaIeta < 0.034)) \
                                     && (subLeadingPhoton.hcalTowerSumEtConeDR03 - 0.005 * subLeadingPhoton.pt < 50.0) \
                                     && (subLeadingPhoton.trkSumPtHollowConeDR03 - 0.002 * subLeadingPhoton.pt < 50.0) \
+                                    && (subLeadingPhoton.getpfChgIsoWrtChosenVtx02 < 4.0) \
                                     )) \
                              ) \
                             ")
           )
-# FIXME Charged PF iso is still left to implement: (leadingPhoton.getChargedPFIso02(getVertex) < 4.0) \
 
