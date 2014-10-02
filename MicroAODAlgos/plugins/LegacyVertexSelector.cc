4540008c65a8a8262f23e6929a813dc3aeccc820
Manuel <manuel.alejandro.olmedo.negrete@cern.ch>
2014-10-02 18:09:19 +0200

    testing gen particle in producer
    fixed some merge conflicts

--- MicroAODAlgos/plugins/LegacyVertexSelector.cc
+++ MicroAODAlgos/plugins/LegacyVertexSelector.cc
@@ -246,7 +246,7 @@ namespace flashgg {
     double dz=-99999;
     double perp = sqrt(conversion->conversionVertex().x()*conversion->conversionVertex().x()+conversion->conversionVertex().y()*conversion->conversionVertex().y());
     if (conversion->nTracks()==2) {
-      if ( fabs(pho->superCluster()->eta()<1.5) ) { // barrel
+      if ( fabs(pho->superCluster()->eta())<1.5 ) { // barrel
 	if ( perp <=15 ) {
 	  dz=sigma1Pix;
 	} else if ( perp > 15 && perp <=60 ) {
