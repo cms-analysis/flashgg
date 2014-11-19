d46b845c36598550f1fa80c39b4838eba37a6d67
Seth Zenz <seth.zenz@cern.ch>
2014-11-19 17:11:41 +0100

    add back ommitted storage of VBF MVA object in VBFTag

--- TagFormats/src/VBFTag.cc
+++ TagFormats/src/VBFTag.cc
@@ -12,27 +12,30 @@ VBFTag::VBFTag(edm::Ptr<flashgg::DiPhotonCandidate> diPho,edm::Ptr<DiPhotonMVARe
   VBFTag::VBFTag(diPho,*mvaRes,*vbfDiPhoDiJet_mvaRes) {}
 
 VBFTag::VBFTag(edm::Ptr<DiPhotonCandidate> dipho,DiPhotonMVAResult mvares,VBFDiPhoDiJetMVAResult vbfDiPhoDiJet_mvaRes) :
-  DiPhotonTagBase::DiPhotonTagBase(dipho,mvares) {}
+  	DiPhotonTagBase::DiPhotonTagBase(dipho,mvares) {
+  		vbfDiPhoDiJet_mva_result_ = vbfDiPhoDiJet_mvaRes;
+}
 
 const VBFDiPhoDiJetMVAResult VBFTag::VBFDiPhoDiJetMVA() const {
-  return vbfDiPhoDiJet_mva_result_;
-	}
+	return vbfDiPhoDiJet_mva_result_;
+}
 const VBFMVAResult VBFTag::VBFMVA() const {
-  return vbfDiPhoDiJet_mva_result_.vbfMvaResult;
-	}
+	return vbfDiPhoDiJet_mva_result_.vbfMvaResult;
+}
 
 const Jet VBFTag::leadingJet() const{
 	return vbfDiPhoDiJet_mva_result_.vbfMvaResult.leadJet;
 }
-		const Jet VBFTag::subLeadingJet() const{
-return vbfDiPhoDiJet_mva_result_.vbfMvaResult.subleadJet;
+
+const Jet VBFTag::subLeadingJet() const{
+	return vbfDiPhoDiJet_mva_result_.vbfMvaResult.subleadJet;
 }
 
 VBFTag* VBFTag::clone () const {
-  VBFTag* result = new VBFTag(diPhoton(),diPhotonMVA(),vbfDiPhoDiJet_mva_result_);
-  result->setCategoryNumber(getCategoryNumber());
-  result->setDiPhotonIndex(getDiPhotonIndex());
-  return result;
+	VBFTag* result = new VBFTag(diPhoton(),diPhotonMVA(),vbfDiPhoDiJet_mva_result_);
+	result->setCategoryNumber(getCategoryNumber());
+	result->setDiPhotonIndex(getDiPhotonIndex());
+	return result;
 }
 
 
