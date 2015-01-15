5b019bb0635594127bb0689d833f438c0c24d004
Pasquale Musella <pasquale.musella@cern.ch>
2015-01-15 17:41:36 +0100

    Import CollectionDumper templated class. - based on StringObject parser - manages trees, histograms and workspace creation out of cmssw collections - plugin-based category classification

---
+++ TagAlgos/src/StringHelpers.cc
@@ -0,0 +1,33 @@
+#include "flashgg/TagAlgos/interface/StringHelpers.h"
+
+#include <boost/algorithm/string/replace.hpp>
+
+
+namespace flashgg
+{
+
+	std::string replaceString(const std::string & inp, const std::string & src, const std::string & replace)
+	{ 
+		return boost::replace_all_copy(inp,src,replace); 
+	}	
+	
+	std::string formatString(const std::string & inp, const std::map<std::string,std::string> & replacements)
+	{
+		auto buf = inp;
+		for(auto & replace : replacements) {
+			auto tmp = replaceString(buf,replace.first,replace.second);
+			buf = std::move(tmp);
+		}
+		return buf;
+	}
+	
+}
+
+
+/// std::string replaceString(const std::string & inp, const std::string & src, const std::string & replace)
+/// {
+/// 	auto buf = inp;
+/// 	boost::replace_all(buf,src,replace);
+/// 	return buf;
+/// }
+
