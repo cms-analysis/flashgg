660ed4fa843d79a838fcc1ac496b402237261d55
Pasquale Musella <pasquale.musella@cern.ch>
2015-01-15 17:43:44 +0100

    Added Photon, DiPhoton and DiPhotonTag specialization of CollectionDumper

---
+++ TagAlgos/src/PhotonDumpers.cc
@@ -0,0 +1,13 @@
+#include "flashgg/TagAlgos/interface/PhotonDumpers.h"
+
+#include "flashgg/TagAlgos/interface/PluggableAnalyzer.h"
+
+namespace flashgg {
+	// typedef AnalyzerFactoryImpl<DiPhotonDumper,"DiPhotonDumper"> DiPhotonDumperFactory;
+	namespace fwlite {
+		PLUGGABLE_ANALYZER(PhotonDumper);
+		PLUGGABLE_ANALYZER(DiPhotonDumper);
+		PLUGGABLE_ANALYZER(CutBasedDiPhotonDumper);
+		PLUGGABLE_ANALYZER(DiPhotonTagDumper);
+	}
+}
