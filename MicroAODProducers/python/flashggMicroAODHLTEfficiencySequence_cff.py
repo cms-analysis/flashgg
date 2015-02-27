d94cfd1f763fc6e5364648282e466dde4ee0ecaa
Matteo <matteosan1@gmail.com>
2015-02-27 01:03:10 +0100

    rationalize cff files

--- MicroAODProducers/python/flashggMicroAODHLTEfficiencySequence_cff.py
+++ MicroAODProducers/python/flashggMicroAODHLTEfficiencySequence_cff.py
@@ -1,28 +1,10 @@
 import FWCore.ParameterSet.Config as cms
-from flashgg.MicroAODProducers.flashggTkVtxMap_cfi import flashggVertexMapUnique,flashggVertexMapNonUnique
-from flashgg.MicroAODProducers.flashggPhotons_cfi import flashggPhotons
-from flashgg.MicroAODProducers.flashggDiPhotons_cfi import flashggDiPhotons
+
+from flashgg.MicroAODProducers.flashggMicroAODSequence_cff import *
 from flashgg.MicroAODProducers.flashggHLTDiPhotons_cfi import flashggHLTDiPhotons
-from flashgg.MicroAODProducers.flashggElectrons_cfi import flashggElectrons
-from flashgg.MicroAODProducers.flashggMicroAODGenSequence_cff import *#flashggMicroAODGenSequence
 
-eventCount = cms.EDProducer("EventCountProducer")
-weightsCount = cms.EDProducer("WeightsCountProducer",
-                              generator=cms.InputTag("generator"),
-                              pileupInfo=cms.InputTag("addPileupInfo"),
-                              doObsPileup=cms.untracked.bool(True),
-                              minObsPileup=cms.double(-0.5),
-                              maxObsPileup=cms.double(100.5),
-                              nbinsObsPileup=cms.int32(101),
-                              )
+flashggMicroAODHLTEfficiencySequence = cms.Sequence(flashggMicroAODSequence)
+flashggMicroAODHLTEfficiencySequence.remove(flashggPreselectedDiPhotons)
+flashggMicroAODHLTEfficiencySequence.remove(flashggJets)
+flashggMicroAODHLTEfficiencySequence += (flashggHLTDiPhotons) 
 
-flashggMicroAODHLTEfficiencySequence = cms.Sequence((eventCount+weightsCount
-                                        +flashggVertexMapUnique+flashggVertexMapNonUnique+flashggElectrons
-                                        )
-                                       *flashggMicroAODGenSequence
-                                       *flashggPhotons*flashggDiPhotons
-                                       *(flashggHLTDiPhotons)
-                                       )
-#from flashgg.MicroAODProducers.flashggGenPhotonsExtra_cfi import flashggGenPhotonsExtra
-
-#flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra
--- MicroAODProducers/python/flashggMicroAODZeeValidationSequence_cff.py
+++ MicroAODProducers/python/flashggMicroAODZeeValidationSequence_cff.py
@@ -1,25 +1,10 @@
 import FWCore.ParameterSet.Config as cms
-from flashgg.MicroAODProducers.flashggTkVtxMap_cfi import flashggVertexMapUnique,flashggVertexMapNonUnique
-from flashgg.MicroAODProducers.flashggPhotons_cfi import flashggPhotons
-from flashgg.MicroAODProducers.flashggDiPhotons_cfi import flashggDiPhotons
+
+from flashgg.MicroAODProducers.flashggMicroAODSequence_cff import *
 from flashgg.MicroAODProducers.flashggZeeDiPhotons_cfi import flashggZeeDiPhotons
-from flashgg.MicroAODProducers.flashggJets_cfi import flashggJets
-from flashgg.MicroAODProducers.flashggMicroAODGenSequence_cff import *
 
-eventCount = cms.EDProducer("EventCountProducer")
-weightsCount = cms.EDProducer("WeightsCountProducer",
-                              generator=cms.InputTag("generator"),
-                              pileupInfo=cms.InputTag("addPileupInfo"),
-                              doObsPileup=cms.untracked.bool(True),
-                              minObsPileup=cms.double(-0.5),
-                              maxObsPileup=cms.double(100.5),
-                              nbinsObsPileup=cms.int32(101),
-                              )
-
-flashggMicroAODZeeValidationSequence = cms.Sequence((eventCount+weightsCount
-                                                     +flashggVertexMapUnique+flashggVertexMapNonUnique
-                                                     +flashggMicroAODGenSequence
-                                                     )
-                                                    *flashggPhotons*flashggDiPhotons
-                                                    *(flashggZeeDiPhotons)
-                                                    )
+flashggMicroAODZeeValidationSequence = cms.Sequence(flashggMicroAODSequence)
+flashggMicroAODZeeValidationSequence.remove(flashggElectrons)
+flashggMicroAODZeeValidationSequence.remove(flashggPreselectedDiPhotons)
+flashggMicroAODZeeValidationSequence.remove(flashggJets)
+flashggMicroAODZeeValidationSequence += flashggZeeDiPhotons
