dd11a86b9c15e654dce222e40410c2dc36cc3f98
Pasquale Musella <pasquale.musella@cern.ch>
2015-04-08 10:04:23 +0200

    Added editor's local variables to newly created files

--- MicroAOD/src/IsolationAlgoBase.cc
+++ MicroAOD/src/IsolationAlgoBase.cc
@@ -9,3 +9,11 @@ namespace flashgg {
     {
     }
 }
+// Local Variables:
+// mode:c++
+// indent-tabs-mode:nil
+// tab-width:4
+// c-basic-offset:4
+// End:
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
