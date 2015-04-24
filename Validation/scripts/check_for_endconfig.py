0ac9a518153f4b06c582b11ebf133d54838979ff
Seth Zenz <seth.zenz@cern.ch>
2015-04-24 15:37:09 +0200

    prompt user to add the missing emacs/vim formatting text

--- Validation/scripts/check_for_endconfig.py
+++ Validation/scripts/check_for_endconfig.py
@@ -9,7 +9,8 @@ flashggdir = os.environ["CMSSW_BASE"] + "/src/flashgg"
 if not os.access(flashggdir,os.F_OK):
   raise Exception,"flashgg not set up correctly? no directory %s" % flashggdir  
 
-endtext = open("%s/Validation/scripts/endconfig.txt" % flashggdir,'r').read()
+with open("%s/Validation/scripts/endconfig.txt" % flashggdir,'r') as f:
+  endtext = f.read()
 
 exts = ["cc","h"]
 
@@ -20,7 +21,14 @@ for root, dirnames, filenames in os.walk(flashggdir):
       matches.append(os.path.join(root, filename))
 
 for match in matches:
-  text = open(match,'r').read()
-  if not text.count(endtext):
-    print match
+  with open(match,'r') as f:
+    text = f.read()
+    if not text.count(endtext):
+      answer = ""
+      while answer != "y" and answer != "n":
+        querytext = "File %s is missing the end formatting. Fix it? [y/n] " % match
+        answer = raw_input(querytext).lower()[0]
+        if answer == "y":
+          with open(match,'a') as f:
+            f.write("\n"+endtext)
 
