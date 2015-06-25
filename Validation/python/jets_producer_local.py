ed656cf2d337d73c4a01eb6f93f24647787bcbcf
yacine <yhaddad@cern.ch>
2015-06-25 15:00:22 +0200

    cleaning for PR

--- Validation/python/jets_producer_local.py
+++ Validation/python/jets_producer_local.py
@@ -20,19 +20,14 @@ jdebug=True
 
 # PHYS14 Files
 process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring(
-    #"/store/mc/Phys14DR/DYToMuMu_M-50_Tune4C_13TeV-pythia8/MINIAODSIM/PU40bx25_tsg_castor_PHYS14_25_V1-v2/00000/622CAFBA-BD9A-E411-BE11-002481E14FFC.root",
-    #"/store/mc/Phys14DR/DYToMuMu_M-50_Tune4C_13TeV-pythia8/MINIAODSIM/PU40bx25_tsg_castor_PHYS14_25_V1-v2/00000/FA4B46B9-8E9A-E411-A899-002590A3C954.root",
-    #"/store/mc/Phys14DR/DYToMuMu_M-50_Tune4C_13TeV-pythia8/MINIAODSIM/PU40bx25_tsg_castor_PHYS14_25_V1-v2/10000/8607F88E-F799-E411-A180-0025B3E063F0.root",
-    #"/store/mc/Phys14DR/DYToMuMu_M-50_Tune4C_13TeV-pythia8/MINIAODSIM/PU40bx25_tsg_castor_PHYS14_25_V1-v2/10000/F620A7C9-F799-E411-8DEF-002590A371AC.root"
-    #"/store/mc/Spring14miniaod/VBF_HToGG_M-125_13TeV-powheg-pythia6/MINIAODSIM/141029_PU40bx50_PLS170_V6AN2-v1/10000/5C3A5675-7C72-E411-AC85-003048D436EA.root"
-    "/store/mc/Phys14DR/GluGluToHToGG_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/3C2EFAB1-B16F-E411-AB34-7845C4FC39FB.root"),
-    skipEvents=cms.untracked.uint32(0)
-                       
-)
+    "/store/mc/Phys14DR/GluGluToHToGG_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/3C2EFAB1-B16F-E411-AB34-7845C4FC39FB.root"), 
+                            skipEvents=cms.untracked.uint32(0)
+                            
+                        )
 
 process.MessageLogger.cerr.threshold = 'ERROR' # can't get suppressWarning to work: disable all warnings for now
 # process.MessageLogger.suppressWarning.extend(['SimpleMemoryCheck','MemoryCheck']) # this would have been better...
- 
+
 # Uncomment the following if you notice you have a memory leak
 # This is a lightweight tool to digg further
 process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
@@ -42,11 +37,10 @@ process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
 
 process.load("flashgg/MicroAOD/flashggMicroAODSequence_cff")
 process.load("flashgg/MicroAOD/flashggMicroAODExtraJetsSequence_cff")
-process.load("flashgg/Validation/JetTreeMaker_cff")
 
 from flashgg.MicroAOD.flashggMicroAODOutputCommands_cff import microAODDefaultOutputCommand,microAODDebugOutputCommand
 process.out = cms.OutputModule("PoolOutputModule",
-                               fileName       = cms.untracked.string('.workspace/myMicroAODOutputFile.root'),
+                               fileName       = cms.untracked.string('./workspace/myMicroAODOutputFile.root'),
                                outputCommands = microAODDefaultOutputCommand
                            )
 
@@ -55,31 +49,53 @@ process.out.outputCommands += microAODDebugOutputCommand # extra items for debug
 # need to allow unscheduled processes otherwise reclustering function will fail
 # this is because of the jet clustering tool, and we have to live with it for now.
 # NOTE: new PAT default running is "unscheduled" so we just need to say in the outputCommands what we want to store
+
 process.options = cms.untracked.PSet(
     allowUnscheduled = cms.untracked.bool(True)
 )
 
 # ++++++++++++ PF +++++++++++++++++++
-# uses the vertex 0
-from flashgg.MicroAOD.flashggExtraJets_cfi import addFlashggPF
+# uses the vertex leg
+from flashgg.MicroAOD.flashggExtraJets_cfi import addFlashggPF , addQGTaggerPF
+# call the function, it takes care of everything else.
 addFlashggPF(process)
+# add QG tagger
+addQGTaggerPF(process)
 # +++++++++++++++++++++++++++++++++++
 
 # ++++++++++++ PFCHS0 +++++++++++++++
 # uses the vertex 0
-from flashgg.MicroAOD.flashggExtraJets_cfi import addFlashggPFCHS0
+from flashgg.MicroAOD.flashggExtraJets_cfi import addFlashggPFCHS0 , addQGTaggerPFCHS0
+# call the function, it takes care of everything else.
 addFlashggPFCHS0(process)
+# add QG tagger
+addQGTaggerPFCHS0(process)
 # +++++++++++++++++++++++++++++++++++
 
 # ++++++++++++ PFCHSLeg +++++++++++++
 # import function which takes care of reclustering the jets using legacy vertex     
-from flashgg.MicroAOD.flashggJets_cfi import addFlashggPFCHSLegJets 
+from flashgg.MicroAOD.flashggJets_cfi import addFlashggPFCHSLegJets , addQGTaggerPFCHSLeg
 # call the function, it takes care of everything else.
 addFlashggPFCHSLegJets(process)
+# add QG tagger
+addQGTaggerPFCHSLeg(process)
 # +++++++++++++++++++++++++++++++++++
 
 
-# ++++++++++++ JetTreeMaker +++++++++
+# ++++++++++++ QG Tagger Test ++++++++++++
+#process.load('RecoJets.JetProducers.QGTagger_cfi')
+
+# Could be reco::PFJetCollection or pat::JetCollection (both AOD and miniAOD)
+#process.QGTagger.srcJets     = cms.InputTag('flashggJetsPFCHS0')
+#process.QGTagger.jetsLabel   = cms.string('QGL_AK4PFchs') 
+## for all the options :  https://twiki.cern.ch/twiki/bin/viewauth/CMS/QGDataBaseVersion
+
+#process.QGTaggerPF       = process.QGTagger.clone( srcJets = 'flashggJetsPF'      ,jetsLabel = 'QGL_AK4PF' );
+#process.QGTaggerPFCHS0   = process.QGTagger.clone( srcJets = 'flashggJetsPFCHS0'  ,jetsLabel = 'ak4PFJetsCHS' );
+#process.QGTaggerPFCHSLeg = process.QGTagger.clone( srcJets = 'flashggJetsPFCHSLeg',jetsLabel = 'ak4PFJetsCHS' );
+## +++++++++++++++++++++++++++++++++++
+
+## ++++++++++++ JetTreeMaker +++++++++
 process.TFileService = cms.Service("TFileService",
                                    fileName  = cms.string("./workspace/GluGluToHToGG_M-125_13TeV_JetValTrees_NEW_0.root"))
 
@@ -88,22 +104,25 @@ process.flashggJetValidationTreeMakerPF = cms.EDAnalyzer('FlashggJetValidationTr
                                                          JetTagDz              = cms.InputTag("flashggJetsPF"),
                                                          StringTag	       = cms.string("PF"),
                                                          VertexCandidateMapTag = cms.InputTag("flashggVertexMapUnique"),
+                                                         qgTaggerInput         = cms.untracked.InputTag("QGTaggerPF"),
                                                          debug                 = cms.untracked.bool(jdebug),
                                                      )
 
 process.flashggJetValidationTreeMakerPFCHS0 = cms.EDAnalyzer('FlashggJetValidationTreeMaker',
-                                                             GenParticleTag     = cms.untracked.InputTag('prunedGenParticles'),
-                                                             JetTagDz           = cms.InputTag("flashggJetsPFCHS0"),
-                                                             StringTag		= cms.string("PFCHS0"),
+                                                             GenParticleTag        = cms.untracked.InputTag('prunedGenParticles'),
+                                                             JetTagDz              = cms.InputTag("flashggJetsPFCHS0"),
+                                                             StringTag		   = cms.string("PFCHS0"),
                                                              VertexCandidateMapTag = cms.InputTag("flashggVertexMapUnique"),
+                                                             qgTaggerInput         = cms.untracked.InputTag("QGTaggerPFCHS0"),
                                                              debug                 = cms.untracked.bool(jdebug),
                                                          )
-
+# This producer is the default in flashgg Dijet MVA 
 process.flashggJetValidationTreeMakerPFCHSLeg = cms.EDAnalyzer('FlashggJetValidationTreeMaker',
-                                                               GenParticleTag = cms.untracked.InputTag('prunedGenParticles'),
-                                                               JetTagDz       = cms.InputTag("flashggJets"),                  
-                                                               StringTag	= cms.string("PFCHSLeg"),
+                                                               GenParticleTag        = cms.untracked.InputTag('prunedGenParticles'),
+                                                               JetTagDz              = cms.InputTag("flashggJets"),                  
+                                                               StringTag	     = cms.string("PFCHSLeg"),
                                                                VertexCandidateMapTag = cms.InputTag("flashggVertexMapUnique"),
+                                                               qgTaggerInput         = cms.untracked.InputTag("QGTaggerPFCHSLeg"),
                                                                debug                 = cms.untracked.bool(jdebug),
                                                            )
 
@@ -112,8 +131,8 @@ process.p = cms.Path(  process.flashggMicroAODSequence
                        + process.flashggMicroAODExtraJetsSequence
                        # tree producer ...
                        + process.flashggJetValidationTreeMakerPF 
-                       + process.flashggJetValidationTreeMakerPFCHS0
-                       + process.flashggJetValidationTreeMakerPFCHSLeg
+                       #+ process.flashggJetValidationTreeMakerPFCHS0
+                       #+ process.flashggJetValidationTreeMakerPFCHSLeg
                    )
 
 process.e = cms.EndPath(process.out)
