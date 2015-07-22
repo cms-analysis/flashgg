375a63a8d3b7f5748f0051dfb691973954c5b8f6
Seth Zenz <seth.zenz@cern.ch>
2015-07-22 10:31:49 +0200

    rename PhotonCollectionFromDiPhotons because it is becoming more general

--- MicroAOD/python/flashggSlimmedPhotonAndDiPhoton_cfi.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-flashggSlimmedPhotonAndDiPhoton = cms.EDProducer("FlashggPhotonCollectionFromDiPhotons",
-                                                 DiPhotonTag=cms.InputTag("flashggPreselectedDiPhotons"))
