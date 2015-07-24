625b96e112b1342f7a8679360e0742d99ade1892
yacine <yhaddad@cern.ch>
2015-07-24 15:57:56 +0200

    major changes on the jet tools

--- MicroAOD/plugins/MultiCHSLegVertexCandProducer.cc
+++ MicroAOD/plugins/MultiCHSLegVertexCandProducer.cc
@@ -1,3 +1,8 @@
+/** MultiCHSLegVertexCandProducer.cc ---
+ *
+ * Author: yhaddad <yhaddad@cern.ch>
+ *
+ */
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -13,43 +18,41 @@
 #include "DataFormats/Math/interface/deltaR.h"
 #include "flashgg/DataFormats/interface/DiPhotonCandidate.h"
 
-
 using namespace edm;
 using namespace std;
 
 namespace flashgg {
-
-    class CHSLegacyVertexCandidateProducer : public EDProducer
+    class MultiCHSLegacyVertexCandProducer : public EDProducer
     {
 
     public:
-        CHSLegacyVertexCandidateProducer( const ParameterSet & );
+        MultiCHSLegacyVertexCandProducer( const ParameterSet & );
     private:
         void produce( Event &, const EventSetup & ) override;
-        EDGetTokenT<View<reco::Vertex> > vertexToken_;
+        EDGetTokenT<View<reco::Vertex> >               vertexToken_;
         EDGetTokenT<View<flashgg::DiPhotonCandidate> > diPhotonsToken_;
-        EDGetTokenT<View<pat::PackedCandidate> > pfcandidateToken_;
-        EDGetTokenT< VertexCandidateMap > vertexCandidateMapToken_;
-        //			double maxAllowedDz_;
-        //			bool useEachTrackOnce_;
+        EDGetTokenT<View<pat::PackedCandidate> >       pfcandidateToken_;
+        EDGetTokenT< VertexCandidateMap >              vertexCandidateMapToken_;
 
-        bool useZeroth;
+        unsigned int  indexVtx_;
+        bool debug_;
+        unsigned int eventNb;
     };
 
-    CHSLegacyVertexCandidateProducer::CHSLegacyVertexCandidateProducer( const ParameterSet &iConfig ) :
+    MultiCHSLegacyVertexCandProducer::MultiCHSLegacyVertexCandProducer( const ParameterSet &iConfig ) :
         vertexToken_( consumes<View<reco::Vertex> >( iConfig.getParameter<InputTag> ( "VertexTag" ) ) ),
         diPhotonsToken_( consumes<View<flashgg::DiPhotonCandidate> >( iConfig.getParameter<InputTag> ( "DiPhotonTag" ) ) ),
         pfcandidateToken_( consumes<View<pat::PackedCandidate> >( iConfig.getParameter<InputTag> ( "PFCandidatesTag" ) ) ),
         vertexCandidateMapToken_( consumes<VertexCandidateMap>( iConfig.getParameter<InputTag>( "VertexCandidateMapTag" ) ) ),
-        useZeroth( iConfig.getUntrackedParameter<bool>( "UseZeroth", false ) )
+        indexVtx_( iConfig.getUntrackedParameter<unsigned int> ( "vertexIndex",  0 ) ),
+        debug_( iConfig.getUntrackedParameter<bool>( "debug"      ,  false ) )
     {
         produces<vector<pat::PackedCandidate> >();
+        eventNb = 0 ;
     }
 
-
-    void CHSLegacyVertexCandidateProducer::produce( Event &evt , const EventSetup & )
+    void MultiCHSLegacyVertexCandProducer::produce( Event &evt , const EventSetup & )
     {
-
         // diphotons)
         Handle<View<flashgg::DiPhotonCandidate> > diPhotons;
         evt.getByToken( diPhotonsToken_, diPhotons );
@@ -74,31 +77,29 @@ namespace flashgg {
         VertexCandidateMap vtxmapbycand( *vtxmap );
         std::stable_sort( vtxmapbycand.begin(), vtxmapbycand.end(), flashgg::compare_by_cand() );
 
-        edm::Ptr<reco::Vertex> flashVertex;
 
-        //std::cout <<"Run[" << evt.run() <<  "]=evt["<< evt.id().event() << "]\t npart::" <<  pfCandidates->size() << std::endl;
-        if( useZeroth ) { flashVertex =  primaryVertices->ptrAt( 0 ); }
+        edm::Ptr<reco::Vertex> choosenVertex;
+
+        // force the use of the MiniAOD default vertex
+
+        if( indexVtx_ == 0 ) { choosenVertex =  primaryVertices->ptrAt( 0 ); }
+
+
+        // Use the other vertices :
+        // we are interested only by the di-photon candidates verticies
+        // then retain only the vertex of the di-photon that matters
+
         else {
-            if( diPhotons->size() == 0 ) {
-                flashVertex = primaryVertices->ptrAt( 0 );
-            }
-            if( diPhotons->size() == 1 ) {
-                flashVertex = diPhotons->ptrAt( 0 )->vtx();
-            }
-            if( diPhotons->size() > 1 ) { //hopefully very rare
-                flashVertex = diPhotons->ptrAt( 0 )->vtx();
-            }
 
+            // In the Old producer the case where there is no di-photon use
+            // the MiniAOD default vertex. In the MutiCHS the go is to store
+            // an empty collection --- > for now keeping the old version
 
-            for( unsigned int diPhoLoop = 0; diPhoLoop < diPhotons->size() ; diPhoLoop++ ) {
-                //if( diPhotons[diPhoLoop]->vertex()->position() != flashVertex->position()){
-                // we only have a problem if the mutliple diphotons haev different vertices...
-                if( diPhotons->ptrAt( diPhoLoop )->vtx() != flashVertex ) {
-                    //replace with Error Logger at some stage, cout is not thread safe.
-                    std::cout << "[WARNING] Multiple Diphotons in event, with different PVs "
-                              << ". Using 0th Diphoton Vtx for CHS PU subtraction." << std::endl;
-                    break;
-                }
+            //if ( diPhotons->size() == 0 ){
+            //choosenVertex = primaryVertices->ptrAt( 0 );
+            //}
+            if( diPhotons->size() > indexVtx_ ) {
+                choosenVertex = diPhotons->ptrAt( indexVtx_ - 1 )->vtx();
             }
         }
 
@@ -117,22 +118,22 @@ namespace flashgg {
             auto mapRange = std::equal_range( vtxmapbycand.begin(), vtxmapbycand.end(), cand, flashgg::compare_with_cand() );
             for( auto pair_iter = mapRange.first ; pair_iter != mapRange.second ; pair_iter++ ) {
                 edm::Ptr<reco::Vertex> currentVertex = pair_iter->first;
-                if( flashVertex == currentVertex ) { result->push_back( *cand ); }
+                if( choosenVertex == currentVertex ) { result->push_back( *cand ); }
             }
         }
 
-
+        if( debug_ ) std::cout << setw( 12 ) << "event nb::" << eventNb
+                                   << setw( 12 ) << "dipho id::" << diPhotons->size()
+                                   << setw( 12 ) << "diphoton::" << diPhotons->size()
+                                   << setw( 12 ) << "prim vtx::" << primaryVertices->size()
+                                   << setw( 12 ) << "nbr cand::" << result->size()
+                                   << std::endl;
 
         evt.put( result );
+        eventNb++;
     }
-}
-typedef flashgg::CHSLegacyVertexCandidateProducer FlashggCHSLegacyVertexCandidateProducer;
-DEFINE_FWK_MODULE( FlashggCHSLegacyVertexCandidateProducer );
-// Local Variables:
-// mode:c++
-// indent-tabs-mode:nil
-// tab-width:4
-// c-basic-offset:4
-// End:
-// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
+}// end flashgg namespace
+
+typedef flashgg::MultiCHSLegacyVertexCandProducer FlashggMultiCHSLegacyVertexCandProducer;
+DEFINE_FWK_MODULE( FlashggMultiCHSLegacyVertexCandProducer );
--- MicroAOD/python/flashggJets_cfi.py
+++ MicroAOD/python/flashggJets_cfi.py
@@ -7,83 +7,70 @@ from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
 
 flashggBTag = 'pfCombinedInclusiveSecondaryVertexV2BJetTags'
 
-def addFlashggPFCHSLegJets(process):
 
-  # NOTE : these lines are not longer needed
-  # load various necessary plugins.
-  ## process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
-  ## process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
+def addFlashggPFCHSLegJets(process, vertexIndex_ = 1, doQGTagging = True, label ='', postfix='postfix'):
+  postfix += label 
   
-  # leptons to remove as per default CHS workflow
-  # select the isolated leptons : electrons + muons
+  setattr(process, 'selectedMuons' + postfix, cms.EDFilter("CandPtrSelector", 
+                                                           src = cms.InputTag("slimmedMuons"), 
+                                                           cut = cms.string('''abs(eta)<2.5 && pt>10. &&
+                                                           (pfIsolationR04().sumChargedHadronPt+
+                                                           max(0.,pfIsolationR04().sumNeutralHadronEt+
+                                                           pfIsolationR04().sumPhotonEt-
+                                                           0.50*pfIsolationR04().sumPUPt))/pt < 0.20 && 
+                                                           (isPFMuon && (isGlobalMuon || isTrackerMuon) )''')))
   
-  process.selectedMuons     = cms.EDFilter("CandPtrSelector", 
-                                           src = cms.InputTag("slimmedMuons"), 
-                                           cut = cms.string('''abs(eta)<2.5 && pt>10. &&
-                                           (pfIsolationR04().sumChargedHadronPt+
-                                           max(0.,pfIsolationR04().sumNeutralHadronEt+
-                                           pfIsolationR04().sumPhotonEt-
-                                           0.50*pfIsolationR04().sumPUPt))/pt < 0.20 && 
-                                           (isPFMuon && (isGlobalMuon || isTrackerMuon) )'''))
-  
-  process.selectedElectrons = cms.EDFilter("CandPtrSelector", 
-                                           src = cms.InputTag("slimmedElectrons"), 
-                                           cut = cms.string('''abs(eta)<2.5 && pt>20. &&
-                                           gsfTrack.isAvailable() &&
-                                           gsfTrack.hitPattern().numberOfLostHits(\'MISSING_INNER_HITS\') < 2 &&
-                                           (pfIsolationVariables().sumChargedHadronPt+
-                                           max(0.,pfIsolationVariables().sumNeutralHadronEt+
-                                           pfIsolationVariables().sumPhotonEt-
-                                           0.5*pfIsolationVariables().sumPUPt))/pt < 0.15'''))
+  setattr(process, 'selectedElectrons' + postfix, cms.EDFilter("CandPtrSelector", 
+                                                               src = cms.InputTag("slimmedElectrons"), 
+                                                               cut = cms.string('''abs(eta)<2.5 && pt>20. &&
+                                                               gsfTrack.isAvailable() &&
+                                                               gsfTrack.hitPattern().numberOfLostHits(\'MISSING_INNER_HITS\') < 2 &&
+                                                               (pfIsolationVariables().sumChargedHadronPt+
+                                                               max(0.,pfIsolationVariables().sumNeutralHadronEt+
+                                                               pfIsolationVariables().sumPhotonEt-
+                                                               0.5*pfIsolationVariables().sumPUPt))/pt < 0.15''')))
   
   # Simple producer which just removes the Candidates which
   # don't come from the legacy vertex according to the Flashgg Vertex Map
-  process.flashggCHSLegacyVertexCandidates = cms.EDProducer('FlashggCHSLegacyVertexCandidateProducer',
-                                                            PFCandidatesTag       = cms.InputTag('packedPFCandidates'),
-                                                            DiPhotonTag           = cms.InputTag('flashggDiPhotons'),
-                                                            VertexCandidateMapTag = cms.InputTag("flashggVertexMapForCHS"),
-                                                            VertexTag = cms.InputTag('offlineSlimmedPrimaryVertices')
-                                                          )
-  
-  process.pfCHSLeg = cms.EDFilter("CandPtrSelector", 
-                                  src = cms.InputTag("flashggCHSLegacyVertexCandidates"), 
-                                  cut = cms.string(""))
+  setattr(process,'flashggCHSLegacyVertexCandidates' + postfix,
+          cms.EDProducer('FlashggMultiCHSLegacyVertexCandidateProducer',
+                         PFCandidatesTag       = cms.InputTag('packedPFCandidates'),
+                         DiPhotonTag           = cms.InputTag('flashggDiPhotons'),
+                         VertexCandidateMapTag = cms.InputTag("flashggVertexMapForCHS"),
+                         VertexTag             = cms.InputTag('offlineSlimmedPrimaryVertices'),
+                         vertexIndex           = cms.untracked.int(vertexIndex_)
+                       )
+  )
+  setattr(process, 'pfCHSLeg' + postfix, cms.EDFilter("CandPtrSelector", 
+                                                      src = cms.InputTag('flashggCHSLegacyVertexCandidates' + postfix), 
+                                                      cut = cms.string('')))
   
   # then remove the previously selected muons
-  process.pfNoMuonCHSLeg      = cms.EDProducer("CandPtrProjector", 
-                                               src  = cms.InputTag("pfCHSLeg"), 
-                                               veto = cms.InputTag("selectedMuons"))
+  setattr(process, 'pfNoMuonCHSLeg' + postfix,  cms.EDProducer("CandPtrProjector", 
+                                                               src  = cms.InputTag("pfCHSLeg" + postfix), 
+                                                               veto = cms.InputTag("selectedMuons" + postfix)))
   # then remove the previously selected electrons
-  process.pfNoElectronsCHSLeg = cms.EDProducer("CandPtrProjector", 
-                                               src  = cms.InputTag("pfNoMuonCHSLeg"), 
-                                               veto = cms.InputTag("selectedElectrons"))
+  setattr(process, 'pfNoElectronsCHSLeg' + postfix,  cms.EDProducer("CandPtrProjector", 
+                                                                    src  = cms.InputTag("pfNoMuonCHSLeg" + postfix), 
+                                                                    veto = cms.InputTag("selectedElectrons" + postfix)))
   
   #Import RECO jet producer for ak4 PF and GEN jet
   from RecoJets.JetProducers.ak4PFJets_cfi  import ak4PFJets
   #from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
-  process.ak4PFJetsCHSLeg = ak4PFJets.clone ( src = 'pfNoElectronsCHSLeg', doAreaFastjet = True)
+  setattr(process, 'ak4PFJetsCHSLeg' + postfix, ak4PFJets.clone ( src = 'pfNoElectronsCHSLeg' + postfix, doAreaFastjet = True))
   #process.ak4GenJetsLeg   = ak4GenJets.clone( src = 'packedGenParticles')
   
-  ## PM: Took out as this was interphering with the global tag settings
-  ## # NOTE : these line are from the new Jet recipe 
-  ## # The following is make patJets, but EI is done with the above
-  ## process.load("Configuration.StandardSequences.MagneticField_cff")
-  ## process.load("Configuration.Geometry.GeometryRecoDB_cff")
-  ## process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-  
-  # cluster the jets
-  # NOTE: this is the 74X recipe for the jet clustering 
+  # NOTE: this is the 74X recipe for the jet clustering
   addJetCollection(
     process,
+    postfix        = postfix,
     labelName      = 'AK4PFCHSLeg',
-    jetSource      = cms.InputTag('ak4PFJetsCHSLeg'),
+    jetSource      = cms.InputTag('ak4PFJetsCHSLeg' + postfix),
     pvSource       = cms.InputTag('offlineSlimmedPrimaryVertices'),
     pfCandidates   = cms.InputTag('packedPFCandidates'),
     svSource       = cms.InputTag('slimmedSecondaryVertices'),
     btagDiscriminators = [ flashggBTag ],
     jetCorrections = ('AK4PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'], 'None'),
-    
-    #genJetCollection = cms.InputTag('ak4GenJetsLeg'),
     genJetCollection = cms.InputTag('slimmedGenJets'),
     genParticles     = cms.InputTag('prunedGenParticles'),
     # jet param
@@ -91,28 +78,26 @@ def addFlashggPFCHSLegJets(process):
   )
   
   #adjust PV used for Jet Corrections
-  process.patJetCorrFactorsAK4PFCHSLeg.primaryVertices = "offlineSlimmedPrimaryVertices"
+  #process.patJetCorrFactorsAK4PFCHSLeg.primaryVertices = "offlineSlimmedPrimaryVertices"
+  getattr(process, 'patJetCorrFactorsAK4PFCHSLeg' + postfix).primaryVertices = "offlineSlimmedPrimaryVertices"
+  # Flashgg Jet producer using the collection created with function above.
+  flashggJets = cms.EDProducer('FlashggJetProducer',
+                               DiPhotonTag = cms.InputTag('flashggDiPhotons'),
+                               VertexTag   = cms.InputTag('offlineSlimmedPrimaryVertices'),
+                               JetTag      = cms.InputTag('patJetsAK4PFCHSLeg' + postfix),
+                               VertexCandidateMapTag = cms.InputTag("flashggVertexMapForCHS"),
+                               PileupJetIdParameters = cms.PSet(pu_jetid)
+  )
+  setattr( process, 'flashggJets' + postfix, flashggJets)
+  #setattr( process, 'selectedFlashggPFCHSLegJet' + postfix, cms.EDFilter("FLASHggJetSelector",
+  #                                                                       src = cms.InputTag("flashggJets" +  postfix),
+  #                                                                       cut = cms.string("pt > 15.")))  
   
-  # adjust MC matching
-  # process.patJetGenJetMatchAK4PFCHSLeg.matched = "ak4GenJetsLeg"
-  # process.patJetPartonMatchAK4PFCHSLeg.matched = "prunedGenParticles"
-  # process.patJetPartons.particles = "prunedGenParticles"
-  
-# Flashgg Jet producer using the collection created with function above.
-flashggJets = cms.EDProducer('FlashggJetProducer',
-                             DiPhotonTag = cms.InputTag('flashggDiPhotons'),
-                             VertexTag   = cms.InputTag('offlineSlimmedPrimaryVertices'),
-                             JetTag      = cms.InputTag('patJetsAK4PFCHSLeg'),
-                             VertexCandidateMapTag = cms.InputTag("flashggVertexMapForCHS"),
-                             PileupJetIdParameters = cms.PSet(pu_jetid)
-)
-  
+  if doQGTagging:
+    from RecoJets.JetProducers.QGTagger_cfi import QGTagger
+    setattr( process, 'QGTaggerPFCHSLeg' + postfix,  QGTagger.clone( srcJets   = 'flashggJets' ,jetsLabel = 'ak4PFJetsCHS'))
+    
+  return getattr( process, 'flashggJets' + postfix)
 
 
-def addQGTaggerPFCHSLeg(process):
-  process.load('RecoJets.JetProducers.QGTagger_cfi')
-  process.QGTaggerPFCHSLeg                  = process.QGTagger.clone( srcJets   = 'flashggJetsPFCHSLeg', jetsLabel = 'ak4PFJetsCHS')
 
-  #process.QGTaggerPFCHSLeg.jec              = cms.InputTag('')# keept empty, because are already corrected
-  #process.QGTaggerPFCHSLeg.systematicsLabel = cms.string('')# Produce systematic smearings (not yet available, keep empty)
-  
