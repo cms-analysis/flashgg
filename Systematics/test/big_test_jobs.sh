fad6a24ba4ab9de8224f0d1f99bf75397bd8855b
Seth Zenz <seth.zenz@cern.ch>
2015-09-07 15:50:35 +0200

    include DY in datasets, update test jobs

--- Systematics/test/big_test_jobs.sh
+++ Systematics/test/big_test_jobs.sh
@@ -1,1 +1,1 @@
-fggRunJobs.py --load big_test_jobs.json -d big_test_jobs -x cmsRun MicroAODtoWorkspace.py maxEvents=-1 -n 50 -q 1nd -D -P useAAA=1
+fggRunJobs.py --load big_test_jobs.json -d big_test_jobs -x cmsRun MicroAODtoWorkspace.py maxEvents=-1 -n 50 -q 1nd -D -P -H
