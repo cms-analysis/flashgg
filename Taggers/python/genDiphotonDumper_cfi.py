2784fd0ddebc9246d8d07e86a200a61d6dfd9bff
Pasquale Musella <pasquale.musella@cern.ch>
2015-09-25 18:40:31 +0200

    fixes for mc-truth photon

--- Taggers/python/genDiphotonDumper_cfi.py
+++ Taggers/python/genDiphotonDumper_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from flashgg.Taggers.genDiphotonDumpConfig_cff import genDiphotonDumpConfig
 
-genDiphotonDumper = cms.EDAnalyzer('CutBasedGenDiphotonDumper',
+genDiphotonDumper = cms.EDAnalyzer('CutBasedGenDiPhotonDumper',
                                 **genDiphotonDumpConfig.parameters_()
                                 )
 
