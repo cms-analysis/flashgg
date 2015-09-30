b56c88bd79d812d68372378469484cd8bb1194c5
Louie <louie.dartmoor.corpe@cern.ch>
2015-09-30 12:51:35 +0200

    added IntLumi variable into workspaces. Can be specified in dumper config if runnign locally or gets taken from over-written by targetLumi if running using fggRunJobs

--- Taggers/python/tagsDumpConfig_cff.py
+++ Taggers/python/tagsDumpConfig_cff.py
@@ -6,7 +6,8 @@ tagsDumpConfig = cms.PSet(
     generatorInfo = cms.InputTag("generator"),
     processId = cms.string(""),
     maxCandPerEvent = cms.int32(-1),
-    lumiWeight = cms.double(1.0),
+    lumiWeight = cms.double(1.0),# Over-written by metaData if using fggRunJobs
+    intLumi = cms.untracked.double(1000.), # in /pb. to be over-written by metaData if using fggRunJobs
     classifierCfg = cms.PSet(categories=cms.VPSet()),
     categories = cms.VPSet(),
 
