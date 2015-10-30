c0c1ee3c78db3cb95f9fcb9de4cc74f51d3fce50
Manuel <manuel.alejandro.olmedo.negrete@cern.ch>
2015-10-30 04:10:30 +0100

    centralization of electron effective areas with recoegamma & updated PDFWeightObjectProducer to be compatble with both powheg and madgraph

--- MicroAOD/python/flashggPDFWeightObject_cfi.py
+++ MicroAOD/python/flashggPDFWeightObject_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 flashggPDFWeightObject = cms.EDProducer('FlashggPDFWeightProducer',
 		tag = cms.untracked.string("initrwgt"),
-		pdfset = cms.untracked.string("PDF_variation"),
+		pdfset = cms.untracked.string("292201"),
 		delimiter_1 = cms.untracked.string("id=\""),
 		delimiter_2 = cms.untracked.string("\">"),
-		delimiter_3 = cms.untracked.string("</weightgroup>"),
+		delimiter_3 = cms.untracked.string("292207"),
 		LHEEventTag = cms.untracked.InputTag('externalLHEProducer'),
 		)
