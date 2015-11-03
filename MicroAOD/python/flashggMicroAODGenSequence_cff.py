b7c2b89184177696e273ce9e5b21b7c14bef70a8
Seth Zenz <seth.zenz@cern.ch>
2015-11-03 16:42:44 +0100

    move pdf weight obj into gen sequence so data customization removes it

--- MicroAOD/python/flashggMicroAODGenSequence_cff.py
+++ MicroAOD/python/flashggMicroAODGenSequence_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from flashgg.MicroAOD.flashggPrunedGenParticles_cfi import flashggPrunedGenParticles
 from flashgg.MicroAOD.flashggGenPhotons_cfi import flashggGenPhotons
 from flashgg.MicroAOD.flashggGenPhotonsExtra_cfi import flashggGenPhotonsExtra
+from flashgg.MicroAOD.flashggPDFWeightObject_cfi import flashggPDFWeightObject
 
-
-flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra
+flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra+flashggPDFWeightObject
                                         )
