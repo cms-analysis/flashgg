5de227314c56457ac52e7d3d1ba77d7c9ac9e26a
vtavolar <vittorio.tavolaro@cern.ch>
2015-11-27 01:43:54 +0100

    adding missing features from flashggDiPhotonMVA_cfi.py

--- Taggers/python/flashggDiPhotonMVA_cfi.py
+++ Taggers/python/flashggDiPhotonMVA_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-flashggDiPhotonMVA = cms.EDProducer('FlashggDiPhotonMVAProducer',
-#                                    DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
-                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
+flashggDiPhotonMVAold = cms.EDProducer('FlashggDiPhotonMVAProducer',
+                                    DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
+#                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
                                     BeamSpotTag=cms.InputTag('offlineBeamSpot'),
                                     diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/HggBambu_SMDipho_Oct29_rwgtptallsigevenbkg7TeV_BDTG.weights.xml"),
                                     VertexProbParamsConv=cms.vdouble(-0.049,-0.241,-0.505,-0.270),
@@ -10,9 +10,9 @@ flashggDiPhotonMVA = cms.EDProducer('FlashggDiPhotonMVAProducer',
 				    Version=cms.string("old")
                                     )
 
-flashggDiPhotonMVANew = cms.EDProducer('FlashggDiPhotonMVAProducer',
-#                                    DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
-                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
+flashggDiPhotonMVA = cms.EDProducer('FlashggDiPhotonMVAProducer',
+                                    DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
+#                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
                                     BeamSpotTag=cms.InputTag('offlineBeamSpot'),
                                     diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_BDTG.weights.xml"),
 			            VertexProbParamsConv=cms.vdouble(-0.049,-0.241,-0.505,-0.270),
