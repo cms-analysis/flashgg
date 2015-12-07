97b584be450cadd07badbc349a6c33acf7217868
Seth Zenz <seth.zenz@cern.ch>
2015-12-07 15:58:25 +0100

    add missing python files

--- Systematics/python/flashggJetSystematics_cfi.py
+++ Systematics/python/flashggJetSystematics_cfi.py
@@ -1,5 +1,26 @@
 import FWCore.ParameterSet.Config as cms
 
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetResolution#JER_Scaling_factors_and_Uncertai
+# Data/MC SFs
+smearBins = cms.PSet(
+    variables = cms.vstring("abs(eta)"),
+    bins = cms.VPSet(
+                     cms.PSet( lowBounds = cms.vdouble(0.0), upBounds = cms.vdouble(0.8),
+                               values = cms.vdouble( 1.061 ), uncertainties = cms.vdouble( 0.023 ) ),
+                     cms.PSet( lowBounds = cms.vdouble(0.8), upBounds = cms.vdouble(1.3),
+                               values = cms.vdouble( 1.088 ), uncertainties = cms.vdouble( 0.029 ) ),
+                     cms.PSet( lowBounds = cms.vdouble(1.3), upBounds = cms.vdouble(1.9),
+                              values = cms.vdouble( 1.106 ), uncertainties = cms.vdouble( 0.030 ) ),
+                     cms.PSet( lowBounds = cms.vdouble(1.9), upBounds = cms.vdouble(2.5),
+                              values = cms.vdouble( 1.126 ), uncertainties = cms.vdouble( 0.094 ) ),
+                     cms.PSet( lowBounds = cms.vdouble(2.5), upBounds = cms.vdouble(3.0),
+                              values = cms.vdouble( 1.343 ), uncertainties = cms.vdouble( 0.123 ) ),
+                     cms.PSet( lowBounds = cms.vdouble(3.0), upBounds = cms.vdouble(3.2),
+                              values = cms.vdouble( 1.303 ), uncertainties = cms.vdouble( 0.111 ) ),
+                     cms.PSet( lowBounds = cms.vdouble(3.2), upBounds = cms.vdouble(5.0),
+                              values = cms.vdouble( 1.320 ), uncertainties = cms.vdouble( 0.286 ) )
+                    ))
+
 def createJetSystematicsForTag(process,jetInputTag):
   num = jetInputTag.productInstanceLabel
   newName = 'flashggJetSystematics'+num
@@ -10,10 +31,18 @@ def createJetSystematicsForTag(process,jetInputTag):
                          SystMethods = cms.VPSet(cms.PSet( MethodName = cms.string("FlashggJetEnergyCorrector"),
                                                            Label = cms.string("JEC"),
                                                            NSigmas = cms.vint32(-1,1),
-                                                           OverallRange = cms.string("1"),
+                                                           OverallRange = cms.string("abs(eta)<5.0"),
                                                            Debug = cms.untracked.bool(False)
+                                                           ),
+                                                 cms.PSet( MethodName = cms.string("FlashggJetSmearConstant"),
+                                                           Label = cms.string("JER"),
+                                                           NSigmas = cms.vint32(-1,1),
+                                                           OverallRange = cms.string("abs(eta)<5.0"),
+                                                           BinList = smearBins,
+                                                           Debug = cms.untracked.bool(False),
                                                            )
                                                  )
+                         
                          )
           )
   setattr(process.RandomNumberGeneratorService,newName,cms.PSet(initialSeed = cms.untracked.uint32(int(num)))) 
