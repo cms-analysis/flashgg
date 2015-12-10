5d1c29278ed4af17bab3dffc04961fea779be2a4
Seth Zenz <seth.zenz@cern.ch>
2015-12-10 01:26:35 +0100

    turn off RV systematic, copy and include central weight for all shifts, default bin for all weights

--- Systematics/python/SystematicDumperDefaultVariables.py
+++ Systematics/python/SystematicDumperDefaultVariables.py
@@ -3,7 +3,7 @@ minimalVariables = ["CMS_hgg_mass[160,100,180]:=diPhoton().mass",
                                                                                #when doing systematics, variables need to have a binning
                                                                                #specified, otherwise the rooDataHist end up empty.
             								       #an assert in the code prevents you from doing this.
-                    "centralObjectWeight := centralWeight"]
+                    "centralObjectWeight[1,-999999.,999999.] := centralWeight"]
 
 minimalHistograms = []
 
@@ -25,5 +25,5 @@ defaultHistograms=["CMS_hgg_mass>>mass(160,100,180)",
                                      "maxEta>>maxEta[0.,0.1,0.2,0.3,0.4,0.6,0.8,1.0,1.2,1.4442,1.566,1.7,1.8,2.,2.2,2.3,2.5]"
                                      ]
 
-systematicVariables=["CMS_hgg_mass[160,100,180]:=diPhoton().mass"]
+systematicVariables=["CMS_hgg_mass[160,100,180]:=diPhoton().mass","centralObjectWeight[1,-999999.,999999.] := centralWeight"]
 systematicHistograms=["CMS_hgg_mass>>mass(160,100,180)"]
