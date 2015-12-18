569f6f8fbeb9c04bf9d2905322bc1c4eb5915f0c
Saranya Ghosh <saranya.ghosh@cern.ch>
2015-12-18 15:20:18 +0100

    Update flashggPrunedGenParticles_cfi.py

--- MicroAOD/python/flashggPrunedGenParticles_cfi.py
+++ MicroAOD/python/flashggPrunedGenParticles_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 flashggPrunedGenParticles = cms.EDProducer("GenParticlePruner",
                                     src = cms.InputTag("prunedGenParticles"),
                                     select = cms.vstring("drop  *  ", # this is the default
+                                                         "keep++ abs(pdgId) = 6",
+                                                         "drop abs(pdgId) > 25",
                                                          "keep status = 3",
                                                          "keep status = 23",
                                                          "keep status = 22",
