eee7fbd4430e73e7f943de0e81ad51ddf613cbf4
Seth Zenz <Seth.Zenz@cern.ch>
2016-01-07 12:12:14 +0000

    vbf tag merger

--- Taggers/plugins/TagMerger.cc
+++ Taggers/plugins/TagMerger.cc
@@ -2,10 +2,13 @@
 #include "CommonTools/UtilAlgos/interface/Merger.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "flashgg/DataFormats/interface/DiPhotonTagBase.h"
+#include "flashgg/DataFormats/interface/VBFTag.h"
 
 typedef Merger<edm::OwnVector<flashgg::DiPhotonTagBase> > TagMerger;
+typedef Merger<std::vector<flashgg::VBFTag> > VBFTagMerger;
 
 DEFINE_FWK_MODULE( TagMerger );
+DEFINE_FWK_MODULE( VBFTagMerger );
 
 // Local Variables:
 // mode:c++
