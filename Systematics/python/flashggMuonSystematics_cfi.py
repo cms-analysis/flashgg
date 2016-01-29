3d69adfa55fb9b0c4389545934fa60517abb37e8
Seth Zenz <seth.zenz@cern.ch>
2016-01-29 16:33:18 +0100

    switching the tool for lepton efficiency weights so that we no longer have any interpolation

--- Systematics/python/flashggMuonSystematics_cfi.py
+++ Systematics/python/flashggMuonSystematics_cfi.py
@@ -5,6 +5,7 @@ binInfo = cms.PSet(
 		bins = cms.VPSet(
 	                # TightID+LoosePFRelative isolation scale factors : SF = SF(ID)*SF(iso|ID)
                         # taken from : https://twiki.cern.ch/twiki/bin/view/CMS/MuonReferenceEffsRun2
+                        cms.PSet(lowBounds = cms.vdouble(0.), upBounds = cms.vdouble(20.), values = cms.vdouble(1.), uncertainties = cms.vdouble(0.,0.)), # This bin should never be selected, just here to avoid crashes
                         cms.PSet(lowBounds = cms.vdouble(20.000000), upBounds = cms.vdouble(25.000000), values = cms.vdouble(0.992659), uncertainties = cms.vdouble(0.014311,0.014311)),
                         cms.PSet(lowBounds = cms.vdouble(25.000000), upBounds = cms.vdouble(30.000000), values = cms.vdouble(0.988595), uncertainties = cms.vdouble(0.014204,0.014204)),
                         cms.PSet(lowBounds = cms.vdouble(30.000000), upBounds = cms.vdouble(40.000000), values = cms.vdouble(0.988147), uncertainties = cms.vdouble(0.014151,0.014151)),
@@ -19,7 +20,7 @@ binInfo = cms.PSet(
 flashggMuonSystematics = cms.EDProducer('FlashggMuonEffSystematicProducer',
 					src = cms.InputTag("flashggSelectedMuons"),
 					SystMethods2D = cms.VPSet(),
-					SystMethods = cms.VPSet(cms.PSet( MethodName = cms.string("FlashggMuonEffScale"),
+					SystMethods = cms.VPSet(cms.PSet( MethodName = cms.string("FlashggMuonWeight"),
 									  Label = cms.string("MuonWeight"),
 									  NSigmas = cms.vint32(-1,1),
 									  OverallRange = cms.string("abs(eta)<2.4"),
