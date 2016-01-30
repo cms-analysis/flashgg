ca261d139c6a7583e13f5f149abfdbc40e7beb88
yhaddad <yhaddad@cern.ch>
2016-01-30 07:29:31 +0000

    dijetMVA+combinedMVA weights update

--- Taggers/python/flashggVBFMVA_cff.py
+++ Taggers/python/flashggVBFMVA_cff.py
@@ -16,7 +16,8 @@ flashggVBFMVA = cms.EDProducer('FlashggVBFMVAProducer',
                                #UseLegacyMVA = cms.bool(True),
                                MinDijetMinv = cms.double(0.0),
                                #vbfMVAweightfile = cms.FileInPath("flashgg/Taggers/data/TMVA_dijet_sherpa_scalewt50_2evenb_powheg200_maxdPhi_oct9_Gradient.weights.xml"),
-                               vbfMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_VBF_CHS_STD_BDTG.weights.xml"),
+                               #vbfMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_VBF_CHS_STD_BDTG.weights.xml"),
+                               vbfMVAweightfile = cms.FileInPath("flashgg/Taggers/data/TMVAClassification_dijetMVA_Jan2016_rmscut_BDTG.weights.xml"),
                                )
 
 # Legacy DiPhoDiJet MVA
@@ -26,5 +27,6 @@ flashggVBFDiPhoDiJetMVA = cms.EDProducer('FlashggVBFDiPhoDiJetMVAProducer',
                                          MVAResultTag=cms.InputTag('flashggDiPhotonMVA'),
                                          UseLegacyMVA = cms.bool(False),
                                          #vbfDiPhoDiJetMVAweightfile = cms.FileInPath("flashgg/Taggers/data/TMVA_vbf_dijet_dipho_evenbkg_scaledwt50_maxdPhi_Gradient.weights.xml"),
-                                         vbfDiPhoDiJetMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoDiJet_CHS_STD_BDTG.weights.xml"),
+                                         #vbfDiPhoDiJetMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoDiJet_CHS_STD_BDTG.weights.xml"),
+                                         vbfDiPhoDiJetMVAweightfile = cms.FileInPath("flashgg/Taggers/data/TMVAClassification_combinedMVA_Jan2016_rmscut_BDTG.weights.xml"),
                                          )
