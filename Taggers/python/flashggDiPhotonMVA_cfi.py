0cace7e174400b9b419e0bc6852a906715ea96b3
vtavolar <vittorio.tavolaro@cern.ch>
2016-02-18 17:08:32 +0100

    specify beam spot width in config to build consistent inputs for dipho mva

--- Taggers/python/flashggDiPhotonMVA_cfi.py
+++ Taggers/python/flashggDiPhotonMVA_cfi.py
@@ -1,20 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
 flashggDiPhotonMVAold = cms.EDProducer('FlashggDiPhotonMVAProducer',
-                                    DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
-#                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
-                                    BeamSpotTag=cms.InputTag('offlineBeamSpot'),
-#                                    diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/TMVA_DiPhoton_BDT_betaV7.weights.xml"),
-                                    diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_Rereco.weights.xml"),
-                                    VertexProbParamsConv=cms.vdouble(-0.049,-0.241,-0.505,-0.270),
-                                    VertexProbParamsNoConv=cms.vdouble(-0.344,-0.091,-0.234,-0.186),
-				    Version=cms.string("old")
-                                    )
+                                       DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
+                                       #                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
+                                       BeamSpotTag=cms.InputTag('offlineBeamSpot'),
+                                       BeamSpotSigma=cms.double(4.24),#value is average in data, set to <0 to use actual values from MC and data
+                                       #                                    diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/TMVA_DiPhoton_BDT_betaV7.weights.xml"),
+                                       diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_Rereco.weights.xml"),
+                                       VertexProbParamsConv=cms.vdouble(-0.049,-0.241,-0.505,-0.270),
+                                       VertexProbParamsNoConv=cms.vdouble(-0.344,-0.091,-0.234,-0.186),
+                                       Version=cms.string("old")
+                                       )
 
 flashggDiPhotonMVA = cms.EDProducer('FlashggDiPhotonMVAProducer',
                                     DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'), # why doesn't this work?
-#                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
+                                    #                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
                                     BeamSpotTag=cms.InputTag('offlineBeamSpot'),
+                                    BeamSpotSigma=cms.double(4.24),#value is average in data, set to <0 to use actual values from MC and data
                                     diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_Rereco.weights.xml"),
 			            VertexProbParamsConv=cms.vdouble(-0.049,-0.241,-0.505,-0.270),
                                     VertexProbParamsNoConv=cms.vdouble(-0.344,-0.091,-0.234,-0.186),
