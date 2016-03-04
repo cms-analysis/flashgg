923c2ef0d42bc8ae32ca08f1379d09e99ed762d0
vtavolar <vittorio.tavolaro@cern.ch>
2016-03-04 19:43:57 +0100

    new systematic plugin applying new phoID syst

--- Systematics/plugins/PhotonMvaTransform.cc
+++ Systematics/plugins/PhotonMvaTransform.cc
@@ -30,11 +30,13 @@ namespace flashgg {
         overall_range_( conf.getParameter<std::string>( "OverallRange" ) )
     {
         correctionFile_ = conf.getParameter<edm::FileInPath>("CorrectionFile");
+        std::cout<<correctionFile_.fullPath().c_str()<<std::endl;
         TFile* f = TFile::Open(correctionFile_.fullPath().c_str());
-        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("transfhebup"))->Clone() );
-        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("transfhebdown"))->Clone() );
-        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("transfheeup"))->Clone() );
-        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("transfheedown"))->Clone() );
+        //        f->Print();
+        corrections_.emplace_back( (TGraph*)((TGraph*) f->Get("trasfhebup"))->Clone());
+        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("trasfhebdown"))->Clone() );
+        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("trasfheeup"))->Clone() );
+        corrections_.emplace_back((TGraph*)((TGraph*) f->Get("trasfheedown"))->Clone() );
         f->Close();
     }
 
@@ -53,12 +55,12 @@ namespace flashgg {
 
     void PhotonMvaTransform::applyCorrection( flashgg::Photon &y, int syst_shift )
     {
+        //        if(syst_shift==0) return;
         if( overall_range_( y ) ) { 
-            // LE TRE RIGHE SOTTO NON CREDO SERVANO MA LE HO LASCIATE PER ESSERE GENERALI
             auto val_err = binContents( y );
             float shift_val = val_err.first[0]; 
             if (!applyCentralValue()) shift_val = 0.;
-
+            //            std::cout<<"syst shift is "<<syst_shift<<std::endl;
             int correctionIndex = -1;
             if (y.isEB()) {
                 if (syst_shift > 0)
@@ -72,53 +74,36 @@ namespace flashgg {
                 else
                     correctionIndex = 3;
             }
-            if (correctionIndex == -1) 
-                std::cerr << "ERROR: not enough corrections defined for IDMVA" << std::endl;
-
-            // QUI SOTTO DEVI LOOPARE SULLE PHOTONID MVA E AGGIORNARE IL VALORE PER TUTTI I VERTICI
-            // IERI PENSAVO DI USARE IL SOLITO METODO di shift INFATTI shift_err E` LA DIFFERENZA FRA I DUE VALORI (NOMINALE, TRASFORMATO)
-            // MA SI PUO` AGGIUNGERE UN NUOVO METODO a Photon.h SE CONVIENE.
-            /* 
-            for id in y.photonIDMVA():           
-               float mvaVal = y.photonIDMVA(vtx);
-               float shift_err = mvaVal - corrections_[correctionIndex]->Eval(mvaVal);            
-               float shift = shift_val + shift_err;    
-               y.shiftAllMvaValuesBy( shift ); // we shift all because we don't have access to the selected vertex                                         
-            */
-             
-        }
-        /*
-            auto val_err = binContents( y );
-            if( val_err.first.size() == 1 && val_err.second.size() == 1 ) { // otherwise no-op because we don't have an entry
-                float shift_val = val_err.first[0];  // e.g. 0 if no central value change
-                if (!applyCentralValue()) shift_val = 0.;
-                float shift_err = val_err.second[0]; // e.g. 0.1
-                float shift = shift_val + syst_shift * shift_err;
-                std::vector<edm::Ptr<reco::Vertex> > keys;
-                if( debug_ ) {
-                    std::cout << "  " << shiftLabel( syst_shift ) << ": Photon has pt= " << y.pt() << " eta=" << y.eta()
-                              << " and we apply an mva shift of " << shift << std::endl;
-                    std::cout << "     MVA VALUES BEFORE: ";
-                    auto beforeMap = y.phoIdMvaD();
-                    for(auto keyval = beforeMap.begin(); keyval != beforeMap.end(); ++keyval) {
-                        keys.push_back( keyval->first );
-                        std::cout << keyval->second << " ";
-                    }
-                    std::cout << std::endl;
+            if (correctionIndex == -1) std::cerr << "ERROR: not enough corrections defined for IDMVA" << std::endl;            
+         
+            auto beforeMap = y.phoIdMvaD();
+            std::vector<edm::Ptr<reco::Vertex> > keys;
+            if( debug_ ) {
+                std::cout << " PhoID MVA Syst shift from transformation : Photon has pt= " << y.pt() << " eta=" << y.eta();
+                std::cout << "     MVA VALUES BEFORE: ";
+                for(auto keyval = beforeMap.begin(); keyval != beforeMap.end(); ++keyval) {
+                    keys.push_back( keyval->first );
+                    std::cout << keyval->second << " ";
                 }
-                y.shiftAllMvaValuesBy( shift ); // we shift all because we don't have access to the selected vertex
-                                                // the others are no longer used at this stage anyway, so it cannot hurt
-                if ( debug_) {
-                    auto afterMap = y.phoIdMvaD();
-                    std::cout << "     MVA VALUES AFTER: ";
-                    for(auto key = keys.begin() ; key != keys.end() ; ++key ) {
-                        std::cout << afterMap.at(*key) << " ";
-                    }
-                    std::cout << std::endl;
+                std::cout << std::endl;
+            }
+            edm::Ptr<reco::Vertex> vtx;
+            float shift =0., mvaVal=0.;
+            for(auto keyval = beforeMap.begin(); keyval != beforeMap.end(); ++keyval) {
+                vtx = keyval->first;
+                mvaVal = keyval->second;
+                shift = shift_val + (mvaVal - corrections_[correctionIndex]->Eval(mvaVal))*abs(syst_shift);
+                y.shiftMvaValueBy(shift, vtx);
+            }
+            if ( debug_) {
+                auto afterMap = y.phoIdMvaD();
+                std::cout << "     MVA VALUES AFTER: ";
+                for(auto key = keys.begin() ; key != keys.end() ; ++key ) {
+                    std::cout << afterMap.at(*key) << " ";
                 }
+                std::cout << std::endl;
             }
         }
-        */
     }
 }
 
