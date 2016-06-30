adfe0ac7306e7228240f865b35c2f4790817b80e
Seth Zenz <Seth.Zenz@cern.ch>
2016-06-30 14:38:53 +0100

    update data_jobs, create tag debugging options, default to Golden10June smearing but allow different files

--- Systematics/test/data_jobs.sh
+++ Systematics/test/data_jobs.sh
@@ -1,2 +1,3 @@
 # NB this command is specific to the configuration at IC and is not gaurenteed elsewhere
-fggRunJobs.py --load data_jobs.json -d /vols/cms/szenz/data_jobs_003 -x cmsRun workspaceStd.py maxEvents=-1 -n 500 -q hepmedium.q -D -P useAAA=1 --no-use-tarball lumiMask=/home/hep/szenz/fromscratch25/CMSSW_8_0_8/src/flashgg/MetaData/work/jsons/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt
+fggRunJobs.py --load data_jobs.json -d /vols/cms/szenz/data_jobs_017 -x cmsRun workspaceStd.py maxEvents=-1 -n 500 -q hepmedium.q -D -P useAAA=0 atIC=1 --no-use-tarball \ 
+lumiMask=/home/hep/szenz/fromscratch25/CMSSW_8_0_8/src/flashgg/MetaData/work/jsons/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt # 2.6/fb
