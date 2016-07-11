dfd9638d3aa1fbebeab26b3ef66a5fe32f912de6
Yacine Haddad <yhaddad@cern.ch>
2016-07-11 23:15:43 +0100

    including PUJID working points / diphotonMVA weights (#691)
    * Impl of PUJID-wps for VBFMVAProducer
    * adding the pujid wps +  diphotonMVA

--- Taggers/python/flashggDiPhotonMVA_cfi.py
+++ Taggers/python/flashggDiPhotonMVA_cfi.py
@@ -17,7 +17,9 @@ flashggDiPhotonMVA = cms.EDProducer('FlashggDiPhotonMVAProducer',
                                     #                                    DiPhotonTag=cms.InputTag('flashggDiPhotons'),
                                     BeamSpotTag=cms.InputTag('offlineBeamSpot'),
                                     BeamSpotSigma=cms.double(3.7),#value is average in data, set to <0 to use actual values from MC and data
-                                    diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_80x.weights.xml"),
+                                    #diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_80x.weights.xml"),
+                                    diphotonMVAweightfile = cms.FileInPath("flashgg/Taggers/data/Flashgg_DiPhoton_ICHEP.weights.xml"),
+                                    
                                     # xml 2015                                    
 #			            VertexProbParamsConv=cms.vdouble(-0.049,-0.241,-0.505,-0.270),
 #                                   VertexProbParamsNoConv=cms.vdouble(-0.344,-0.091,-0.234,-0.186),
