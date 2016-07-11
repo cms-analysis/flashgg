cade5a004be2c117e016446abac2367df001d2b2
Seth Zenz <sethzenz@gmail.com>
2016-07-11 19:01:53 +0200

    recognize new miniAODv2 for pu distribution import (#687)
    * recognize new miniAODv2 for pu distribution import
    * now the problem is multiple matches. this fix is still under test

--- MetaData/python/JobConfig.py
+++ MetaData/python/JobConfig.py
@@ -129,6 +129,7 @@ class JobConfig(object):
         try:
             from SimGeneral.MixingModule.mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU_cfi import mix as mix_2016_80_25ns
             self.pu_distribs["80X_mcRun2_asymptotic_2016"] = mix_2016_80_25ns.input.nbPileupEvents
+            self.pu_distribs["PUSpring16"] = mix_2016_80_25ns.input.nbPileupEvents
         except Exception:
             print "Failed to load 80X mixing, this is expected in 7X!"
             
@@ -255,7 +256,17 @@ class JobConfig(object):
                                 matches = filter(lambda x: x in dsetname, self.pu_distribs.keys() )
                                 print matches
                                 if len(matches) > 1:
-                                    matches = filter(lambda x: x == dsetname, matches)
+                                    print "Multiple matches, check if they're all the same"
+                                    allsame = True
+                                    for i in range(1,len(matches)):
+                                        if self.pu_distribs[matches[0]] != self.pu_distribs[matches[i]]:
+                                            allsame = False
+                                    if allsame:
+                                        print "They're all the same so we just take the 0th one:",matches[0]
+                                        matches = [matches[0]]
+                                    else:
+                                        print "Not all the same... so we return to the old behavior and take an exact match, otherwise leave empty..."
+                                        matches = filter(lambda x: x == dsetname, matches)
                                 if len(matches) != 1:
                                     raise Exception("Could not determine sample pu distribution for reweighting. Possible matches are [%s]. Selected [%s]\n dataset: %s" % 
                                                 ( ",".join(self.pu_distribs.keys()), ",".join(matches), dsetname ) )
