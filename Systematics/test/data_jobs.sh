dc040d73f548ab4f04731bbd8d7aa2a971ad1f8b
Seth Zenz <sethzenz@gmail.com>
2016-07-13 15:36:12 +0200

    cleaned up workspace submission setups  (#700)
    * cleaned up workspace submission setups (only sig_jobs and data_jobs) for all mass points in 2_2_0
    * missing comma
    * need to specify full dataset for vh_125 as well

--- Systematics/test/data_jobs.sh
+++ Systematics/test/data_jobs.sh
@@ -1,5 +1,3 @@
 # NB this command is specific to the configuration at IC and is not gaurenteed elsewhere
-LM=/home/hep/szenz/fromscratch27/CMSSW_8_0_8_patch1/src/flashgg/Systematics/test/Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt # 3.99/fb
-#LM=/home/hep/szenz/fromscratch25/CMSSW_8_0_8/src/flashgg/MetaData/work/jsons/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt # 2.6/fb 
-
-fggRunJobs.py --load data_jobs.json -d /vols/cms/szenz/data_jobs_023 -x cmsRun workspaceStd.py maxEvents=-1 -n 500 -q hepshort.q -D -P useAAA=0 atIC=1 --no-use-tarball lumiMask=${LM}
+LM=/afs/cern.ch/work/s/sethzenz/fromscratch107/CMSSW_8_0_8_patch1/src/flashgg/Systematics/test/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON.txt #6.26/fb
+fggRunJobs.py --load data_jobs.json -d data_jobs_003 -x cmsRun workspaceStd.py maxEvents=-1 -n 500 -q 1nh -D -P useAAA=0 atIC=1 --no-use-tarball lumiMask=${LM}
