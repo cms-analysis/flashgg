7964fb3fb48a6cb6b3674b69e2d3ffb46085a134
kmondal <kuntal.mondal@cern.ch>
2016-07-14 18:47:28 +0530

    Updated shower shape correction file for 6.26 /fb data (#710)
    * Updated shower shape correction file for 6.26 /fb data
    * Fixed the file name in config file

--- Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
+++ Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
@@ -11,7 +11,6 @@ flashggUpdatedIdMVADiPhotons = cms.EDProducer("FlashggDiPhotonWithUpdatedPhoIdMV
 #                                              photonIdMVAweightfile_EE = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_25ns_endcap_MoriondVtx.xml"),
                                               photonIdMVAweightfile_EB = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_barrel_ICHEPvtx.xml"),
                                               photonIdMVAweightfile_EE = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_endcap_ICHEPvtx.xml"),
-
-                                              correctionFile           = cms.FileInPath("flashgg/MicroAOD/data/transformation_80X_v1.root"),
+                                              correctionFile           = cms.FileInPath("flashgg/MicroAOD/data/transformation_80X_v2.root"),
                                               Debug                    = cms.bool(False)
                                               )
