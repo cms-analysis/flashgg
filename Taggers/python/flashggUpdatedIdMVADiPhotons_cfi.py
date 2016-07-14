3ae591ff2f284766f8433545ae7f091722a0730d
vtavolar <vtavolar@users.noreply.github.com>
2016-07-14 16:08:58 +0200

    Superseding #709 (#712)
    * minor changes to submit jobs in t3 (to be tested on lxplus)
    * add xsec for ggH, VBF amcatnlo 120,125,130
    * machinery to have sigmaM/M decorr
    * add tag and categ def for sigmaMxpT
    * printout dataset when running jobs
    * workspace production, use long queue when all events
    * define sigmaMoMpToM tag
    * merge GluGlu125 in catalog
    * added xsec for DiPhoton Sherpa Mgg 40-80
    * update change for workspace submission
    * limit cdf to 1e-06 in sigmaM/M to avoid rare events getting 0
    * restore standard configuration for tagsequence and tagsorter
    * options to run ws for xsec from workspaceStd
    * temporary changes to sig_jobs.sh to submit test on t3
    * update xsecs from master and re-add dipho M40-80
    * temporary changes to sig_jobs.sh
    * revert job submission changes for ch t3
    * by default dump standard workspaces

--- Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
+++ Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
@@ -11,6 +11,7 @@ flashggUpdatedIdMVADiPhotons = cms.EDProducer("FlashggDiPhotonWithUpdatedPhoIdMV
 #                                              photonIdMVAweightfile_EE = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_25ns_endcap_MoriondVtx.xml"),
                                               photonIdMVAweightfile_EB = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_barrel_ICHEPvtx.xml"),
                                               photonIdMVAweightfile_EE = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_endcap_ICHEPvtx.xml"),
+                                              # commenting out this parameter will disable all corrections performed by this module
                                               correctionFile           = cms.FileInPath("flashgg/MicroAOD/data/transformation_80X_v2.root"),
                                               Debug                    = cms.bool(False)
                                               )
