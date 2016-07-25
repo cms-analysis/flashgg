afec77aab8fe347300f86b6e832c0d678b26ceaf
Seth Zenz <sethzenz@gmail.com>
2016-07-25 06:00:36 +0100

    Updates of systematics (#745)
    * e-veto SFs from Zmmg with 12.9/fb data and shower shape correction file v3
    * copy file and config from PR 743 by @kmondal: shower shape corr
    * update material systematics
    * job script housekeeping, nb change to data_jobs.json already there for 741 ws

--- Systematics/python/flashggDiPhotonSystematics_cfi.py
+++ Systematics/python/flashggDiPhotonSystematics_cfi.py
@@ -22,24 +22,24 @@ preselBins = cms.PSet(
         )
     )
 
-# slide 7 of https://indico.cern.ch/event/535800/contributions/2213640/attachments/1295950/1968761/201607ZmmgWith2016data_Update12p9perfb.pdf from the update with 12.9/fb
+# slide 7 of https://indico.cern.ch/event/535800/contributions/2213640/attachments/1295950/1968770/201607ZmmgWith2016data_Update12p9perfb.pdf from the update with 12.9/fb
 electronVetoBins = cms.PSet(
     variables = cms.vstring("abs(superCluster.eta)","r9"),
     bins = cms.VPSet(
-        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.00 ) , upBounds = cms.vdouble( 1.5, 0.85 ) , values = cms.vdouble( 0.9925 ) , uncertainties = cms.vdouble( 0.0045 )  ) ,
+        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.00 ) , upBounds = cms.vdouble( 1.5, 0.85 ) , values = cms.vdouble( 0.9924 ) , uncertainties = cms.vdouble( 0.0045 )  ) ,
         cms.PSet( lowBounds = cms.vdouble( 0.0, 0.85 ) , upBounds = cms.vdouble( 1.5, 999. ) , values = cms.vdouble( 0.9960 ) , uncertainties = cms.vdouble( 0.0010 )  ) ,
-        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.00 ) , upBounds = cms.vdouble( 6.0, 0.90 ) , values = cms.vdouble( 0.9880 ) , uncertainties = cms.vdouble( 0.0142 )  ) ,
-        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.90 ) , upBounds = cms.vdouble( 6.0, 999. ) , values = cms.vdouble( 0.9999 ) , uncertainties = cms.vdouble( 0.0029 )  ) 
+        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.00 ) , upBounds = cms.vdouble( 6.0, 0.90 ) , values = cms.vdouble( 0.9863 ) , uncertainties = cms.vdouble( 0.0138 )  ) ,
+        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.90 ) , upBounds = cms.vdouble( 6.0, 999. ) , values = cms.vdouble( 1.0000 ) , uncertainties = cms.vdouble( 0.0029 )  ) 
         )
     )
 
 FNUFBins = cms.PSet(
     variables = cms.vstring("abs(superCluster.eta)","r9"),
     bins = cms.VPSet(
-        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.00 ) , upBounds = cms.vdouble( 1.5, 0.94 ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble(  0.0005 )  ) ,
-        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.94 ) , upBounds = cms.vdouble( 1.5, 999. ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble( -0.0005 )  ) ,
-        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.00 ) , upBounds = cms.vdouble( 6.0, 0.94 ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble(  0.0005 )  ) ,
-        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.94 ) , upBounds = cms.vdouble( 6.0, 999. ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble( -0.0005 )  )
+        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.00 ) , upBounds = cms.vdouble( 1.5, 0.94 ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble(  0.0007 )  ) ,
+        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.94 ) , upBounds = cms.vdouble( 1.5, 999. ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble( -0.0007 )  ) ,
+        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.00 ) , upBounds = cms.vdouble( 6.0, 0.94 ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble(  0.0007 )  ) ,
+        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.94 ) , upBounds = cms.vdouble( 6.0, 999. ) , values = cms.vdouble( 0. ) , uncertainties = cms.vdouble( -0.0007 )  )
         )
     )
 
@@ -282,6 +282,16 @@ materialBinsRun1 = cms.PSet(
         )
     )
 
+materialBinsICHEP = cms.PSet(
+    variables = cms.vstring("abs(superCluster.eta)","r9"),
+    bins = cms.VPSet(
+        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.0 ), upBounds = cms.vdouble( 1.5, 0.94 ), values = cms.vdouble( 0. ) , uncertainties = cms.vdouble( 0.00062 ) ),
+        cms.PSet( lowBounds = cms.vdouble( 0.0, 0.94 ), upBounds = cms.vdouble( 1.5, 999. ), values = cms.vdouble( 0. ), uncertainties = cms.vdouble( 0.00026 ) ),
+        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.0 ), upBounds = cms.vdouble( 6.0, 0.94  ), values = cms.vdouble( 0. ), uncertainties = cms.vdouble( 0.00085 ) ),
+        cms.PSet( lowBounds = cms.vdouble( 1.5, 0.9 ), upBounds = cms.vdouble( 6.0, 999. ), values = cms.vdouble( 0. ), uncertainties = cms.vdouble( 0.00130 ) )
+        )
+    )
+
 emptyBins = cms.PSet(
     variables = cms.vstring("1"),
     bins = cms.VPSet()
@@ -339,8 +349,8 @@ MaterialCentral = cms.PSet( PhotonMethodName = cms.string("FlashggPhotonScale"),
           MethodName = cms.string("FlashggDiPhotonFromPhoton"),
           Label = cms.string("MaterialCentral"),
           NSigmas = cms.vint32(-1,1),
-          OverallRange = cms.string("abs(superCluster.eta)<1.0"),
-          BinList = materialBinsRun1,
+          OverallRange = cms.string("abs(superCluster.eta)<1.5"),
+          BinList = materialBinsICHEP,
           ApplyCentralValue = cms.bool(False),
           Debug = cms.untracked.bool(False)
           )
@@ -349,8 +359,8 @@ MaterialForward = cms.PSet( PhotonMethodName = cms.string("FlashggPhotonScale"),
           MethodName = cms.string("FlashggDiPhotonFromPhoton"),
           Label = cms.string("MaterialForward"),
           NSigmas = cms.vint32(-1,1),
-          OverallRange = cms.string("abs(superCluster.eta)>=1.0"),
-          BinList = materialBinsRun1,
+          OverallRange = cms.string("abs(superCluster.eta)>=1.5"),
+          BinList = materialBinsICHEP,
           ApplyCentralValue = cms.bool(False),
           Debug = cms.untracked.bool(False)
           )
