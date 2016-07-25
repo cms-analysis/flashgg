7ff452809714fdd0210544458adabb9976690442
Seth Zenz <seth.zenz@cern.ch>
2016-07-25 07:19:34 +0200

    update ws number for consistency

--- Systematics/test/data_jobs.sh
+++ Systematics/test/data_jobs.sh
@@ -3,5 +3,5 @@
 #LM=/afs/cern.ch/work/s/sethzenz/fromscratch107/CMSSW_8_0_8_patch1/src/flashgg/MetaData/work/jsons/Cert_271036-276384_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt #9.17/fb
 queue="8nh"
 LM=/afs/cern.ch/work/s/sethzenz/fromscratch107/CMSSW_8_0_8_patch1/src/flashgg/MetaData/work/jsons/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt
-version="744"
+version="745"
 fggRunJobs.py --load data_jobs.json -d data_jobs_${version} -x cmsRun workspaceStd.py maxEvents=-1 -n 100 -q ${queue} -D -P useAAA=0 atIC=1 doFiducial=False lumiMask=${LM}
