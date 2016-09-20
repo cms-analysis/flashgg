0543b6269f86c12324afca4f78112c3bae488280
Michael <mplaner@cern.ch>
2016-09-20 15:40:26 +0200

    PR updates from Seth

--- MicroAOD/python/flashggMets_cfi.py
+++ MicroAOD/python/flashggMets_cfi.py
@@ -1,10 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 
-process = cms.Process("FLASHggMicroAOD")
-
-
-
 
 flashggMets = cms.EDProducer('FlashggMetProducer',
                              verbose = cms.untracked.bool(False),
