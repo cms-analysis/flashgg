9cafa4db2e1b8df96128a0d77a87de3658d9ca3a
ikucher <inna.kucher@cern.ch>
2017-01-23 12:27:56 +0100

    Photon id weights, photon isolation corrections and shower shape transformations (#798)
    * Photon ID Moriond 2017 weights, photon iso corrected for EE and shower shape transformations
    
    * Pha small clean up
    
    * photon id Moriond 2017 : bug fix in photon producer

--- MicroAOD/python/flashggPhotons_cfi.py
+++ MicroAOD/python/flashggPhotons_cfi.py
@@ -14,9 +14,16 @@ flashggPhotons = cms.EDProducer('FlashggPhotonProducer',
                                 photonIdMVAweightfile_EB = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_barrel_ICHEPvtx.xml"),
                                 photonIdMVAweightfile_EE = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_endcap_ICHEPvtx.xml"),
 
-                                useNewPhoId = cms.bool(False),
-                                photonIdMVAweightfile_EB_new = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_barrel_ICHEPvtx_r9s4EtaWshift_wLowMass.xml"),
-                                photonIdMVAweightfile_EE_new = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_endcap_ICHEPvtx_r9s4EtaWshift_wLowMass.xml"),
+                                useNewPhoId = cms.bool(True),
+                                #photonIdMVAweightfile_EB_new = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_barrel_ICHEPvtx_r9s4EtaWshift_wLowMass.xml"),
+                                #photonIdMVAweightfile_EE_new = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_80X_endcap_ICHEPvtx_r9s4EtaWshift_wLowMass.xml"),
+
+                                effAreasConfigFile = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring16/effAreaPhotons_cone03_pfPhotons_90percentBased.txt"),
+                                phoIsoPtScalingCoeff = cms.vdouble(0.0053,0.0034),
+                                phoIsoCutoff = cms.double(2.5),
+
+                                photonIdMVAweightfile_EB_new = cms.FileInPath("flashgg/MicroAOD/data/HggPhoId_barrel_Moriond2017_wRhoRew.weights.xml"),
+                                photonIdMVAweightfile_EE_new = cms.FileInPath("flashgg/MicroAOD/data/HggPhoId_endcap_Moriond2017_wRhoRew.weights.xml"),
 
                                 useNonZsLazyTools = cms.bool(True),
                                 recomputeNonZsClusterShapes = cms.bool(False),
