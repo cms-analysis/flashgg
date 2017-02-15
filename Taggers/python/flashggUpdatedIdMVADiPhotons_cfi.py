ca17b81711597e8e91d627563b08fe78ec7503e9
kuntal <kuntal.mondal@cern.ch>
2017-02-15 11:35:49 +0100

    Added new transformation file with correction for full5x5r9, full5x5sieie, s4 ratio, eta width and also implemented correction for full5x5sieie

--- Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
+++ Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
@@ -26,6 +26,6 @@ flashggUpdatedIdMVADiPhotons = cms.EDProducer("FlashggDiPhotonWithUpdatedPhoIdMV
 
                                               # commenting out this parameter will disable all corrections performed by this module
                                               #correctionFile           = cms.FileInPath("flashgg/MicroAOD/data/transformation_80X_v4.root"),
-                                              correctionFile           = cms.FileInPath("flashgg/MicroAOD/data/transformation_Moriond17_v1.root"),
+                                              correctionFile           = cms.FileInPath("flashgg/MicroAOD/data/transformation_Moriond17_AfterPreApr_v1.root"),
                                               Debug                    = cms.bool(False)
                                               )
