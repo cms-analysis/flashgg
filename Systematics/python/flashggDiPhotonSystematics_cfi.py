8f5590d841346b2c8b92d37d65e73137db1e6722
Seth Zenz <seth.zenz@cern.ch>
2017-02-21 17:26:38 +0100

    this prefix should be consistent with the mva training

--- Systematics/python/flashggDiPhotonSystematics_cfi.py
+++ Systematics/python/flashggDiPhotonSystematics_cfi.py
@@ -306,7 +306,7 @@ emptySigma = cms.PSet(
 ##scalesAndSmearingsPrefix = cms.string("EgammaAnalysis/ElectronTools/data/Winter_2016_reReco_v1_ele")
 ##scalesAndSmearingsPrefixForSigmaEOverE = cms.string("EgammaAnalysis/ElectronTools/data/Winter_2016_reReco_v1_ele")
 scalesAndSmearingsPrefix = cms.string("EgammaAnalysis/ElectronTools/data/Moriond17_74x_pho")
-scalesAndSmearingsPrefixForSigmaEOverE = cms.string("EgammaAnalysis/ElectronTools/data/Moriond17_74x_pho")
+scalesAndSmearingsPrefixForSigmaEOverE = cms.string("EgammaAnalysis/ElectronTools/data/Winter_2016_reReco_v1_ele")
 
 MCScaleHighR9EB = cms.PSet( PhotonMethodName = cms.string("FlashggPhotonScale"),
           MethodName = cms.string("FlashggDiPhotonFromPhoton"),
