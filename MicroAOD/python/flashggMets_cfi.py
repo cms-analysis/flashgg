febb86231fe9f7494f3ee3a80405464d6e9e931b
mplaner <mplaner@cern.ch>
2017-03-07 15:33:29 +0100

    Topic remini aod met (#891)
    * updated to produce the full MET corrections
    
    * Removed EG only MET; consistent collection names
    
    Removed EG only correction from collection and fixed name so that the full MET EG/muon fix rerun at uAOD would be called flashggMets on data.

--- MicroAOD/python/flashggMets_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-
-flashggMets = cms.EDProducer('FlashggMetProducer',
-                             verbose = cms.untracked.bool(False),
-                             metTag = cms.InputTag('slimmedMETs'),
-                             )
-
