6e3ac5b483ae00eca9f18981480a5019b03d1424
vtavolar <vittorio.tavolaro@cern.ch>
2017-03-11 09:10:59 +0100

    update ws scripts

--- Systematics/test/data_jobs_differential.sh
+++ Systematics/test/data_jobs_differential.sh
@@ -1,8 +1,8 @@
 # NB this command is specific to the configuration on lxplus and is not gaurenteed elsewhere
 #outdir="/afs/cern.ch/work/s/sethzenz/ws/" # can't set absolute path on lsf because we're expecting to stage
-queue="short.q"
+queue="all.q"
 useAAA=1
 #atIC=0 # only effect is setting eos path
-version="data_differential_moriond17_reminiaod"
+version="data_differential_moriond17_reminiaod_v2"
 LM=/mnt/t3nfs01/data01/shome/vtavolar/Flashgg/clean8025/CMSSW_8_0_25/src/flashgg/Systematics/test/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
 fggRunJobs.py --load jobs_diphoMVA_Moriond17_reMiniAOD_data.json  --no-copy-proxy -d data_jobs_$version -x cmsRun workspaceStd.py maxEvents=-1 -n 200 -q $queue -D -P useAAA=$useAAA  doFiducial=True acceptance=NONE lumiMask=${LM}
