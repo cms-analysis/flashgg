ab3a2144b2ee05c9fc879091a00270c4f482bdf4
Martina <martina.malberti@cern.ch>
2017-03-29 15:08:42 +0200

    updates for VHLeptonic tags

--- Taggers/python/tagsDumpers_cfi.py
+++ Taggers/python/tagsDumpers_cfi.py
@@ -20,4 +20,7 @@ dict = {'UntaggedTag': 'untagged',
         'VHLooseTag': 'vh',
         'VHTightTag': 'vh',
         'VHHadronicTag': 'vh',
-        'ZPlusJetTag':'zjet'}
+        'ZPlusJetTag':'zjet',
+        'WHLeptonicTag': 'vh',
+        'VHLeptonicLooseTag': 'vh',
+        'ZHLeptonicTag': 'vh'}
