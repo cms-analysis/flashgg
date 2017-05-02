fdbe768d32312a504d5ae12e3ea2799fd1fdfedb
vtavolar <vittorio.tavolaro@cern.ch>
2017-05-02 11:31:38 +0200

    data jobs submission

--- Systematics/test/data_jobs_differential.sh
+++ Systematics/test/data_jobs_differential.sh
@@ -4,6 +4,6 @@
 queue='long.q -l h_vmem=6g'
 useAAA=1
 #atIC=0 # only effect is setting eos path
-version="data_differential_moriond17_reminiaod_v3_newsysts"
+version="data_differential_moriond17_test_OldCatalog"
 LM=/mnt/t3nfs01/data01/shome/vtavolar/Flashgg/clean8025/CMSSW_8_0_25/src/flashgg/Systematics/test/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
 fggRunJobs.py --load jobs_diphoMVA_Moriond17_reMiniAOD_data.json  --no-copy-proxy -d data_jobs_$version -x cmsRun workspaceStd.py maxEvents=-1 -n 200 -q "$queue" -D -P useAAA=$useAAA  doFiducial=True acceptance=NONE lumiMask=${LM}
