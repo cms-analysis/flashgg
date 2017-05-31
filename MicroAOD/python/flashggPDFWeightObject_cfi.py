321266fc03426e8f1813638c55ab5b620437c5a3
gkrintir <gkrintir@cern.ch>
2017-05-31 12:55:11 +0200

    Tag moriond17 v8 (#890)
    * Added PDF weights for th samples
    
    * Removed un neccesary if
    
    * Restored default behavior
    
    * Remove hardcoded LHEEventTag and LHERunLabel tags
    
    * Set Debug flah to False in flashggPDFWeightObject_cfi
    
    * fixed culprit line

--- MicroAOD/python/flashggPDFWeightObject_cfi.py
+++ MicroAOD/python/flashggPDFWeightObject_cfi.py
@@ -5,9 +5,12 @@ flashggPDFWeightObject = cms.EDProducer('FlashggPDFWeightProducer',
                 GenTag      = cms.InputTag('generator'),
 		tag = cms.untracked.string("initrwgt"),
                 isStandardSample = cms.untracked.bool(True), # set to True for centrally produced Hgg samples, set to False for HH2B2G samples
+                isThqSample = cms.untracked.bool(False), #set to False for centrally produced Hgg samples, set to True for THq samples
                 pdfset = cms.untracked.string("NNPDF30_lo_as_0130_nf_4.LHgrid"), # for HH2B2G samples set here the pdf set name
 		doAlphasWeights = cms.untracked.bool(True), # set to True for centrally produced Hgg samples, set to False for HH2B2G samples 
                 doScaleWeights  = cms.untracked.bool(True),
 		nPdfEigWeights = cms.uint32(60),
-		mc2hessianCSV = cms.FileInPath('PhysicsTools/HepMCCandAlgos/data/NNPDF30_lo_as_0130_hessian_60.csv')
+		mc2hessianCSV = cms.FileInPath('PhysicsTools/HepMCCandAlgos/data/NNPDF30_lo_as_0130_hessian_60.csv'),
+                LHERunLabel = cms.string("externalLHEProducer"),
+                Debug = cms.bool(False) 
 	)
