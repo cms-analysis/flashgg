8d6dc4954f38c8603d5d74e0d5f8791e39379a64
vtavolar <vittorio.tavolaro@cern.ch>
2017-07-24 11:43:19 +0200

    update differential submission scripts

--- Systematics/test/data_jobs_differential.sh
+++ Systematics/test/data_jobs_differential.sh
@@ -2,8 +2,8 @@
 #outdir="/afs/cern.ch/work/s/sethzenz/ws/" # can't set absolute path on lsf because we're expecting to stage
 ###queue="all.q"
 queue='long.q -l h_vmem=6g'
-useAAA=1
+useAAA=0
 #atIC=0 # only effect is setting eos path
 version="data_differential_moriond17_test_OldCatalog"
 LM=/mnt/t3nfs01/data01/shome/vtavolar/Flashgg/clean8025/CMSSW_8_0_25/src/flashgg/Systematics/test/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
-fggRunJobs.py --load jobs_diphoMVA_Moriond17_reMiniAOD_data.json  --no-copy-proxy -d data_jobs_$version -x cmsRun workspaceStd.py maxEvents=-1 -n 200 -q "$queue" -D -P useAAA=$useAAA  doFiducial=True acceptance=NONE lumiMask=${LM}
+fggRunJobs.py --load DoubleEG_ReMiniAOD_2_5_Y.json   -d data_jobs_$version -x cmsRun workspaceStd.py maxEvents=-1 -n 200 -q "$queue" -H -D useAAA=$useAAA --no-copy-proxy  doFiducial=True acceptance=NONE lumiMask=${LM}
