52a4b466e89bfa68f014ba63ca21fb0aa89dea86
Seth Zenz <seth.zenz@cern.ch>
2018-05-30 17:55:02 +0200

    2017 format for htxs for all tags

--- Taggers/plugins/UntaggedTagProducer.cc
+++ Taggers/plugins/UntaggedTagProducer.cc
@@ -12,6 +12,7 @@
 #include "flashgg/DataFormats/interface/UntaggedTag.h"
 #include "flashgg/DataFormats/interface/TagTruthBase.h"
 #include "DataFormats/Common/interface/RefToPtr.h"
+#include "SimDataFormats/HTXS/interface/HiggsTemplateCrossSections.h"
 
 
 #include <vector>
@@ -37,6 +38,7 @@ namespace flashgg {
         EDGetTokenT<View<DiPhotonMVAResult> > mvaResultToken_;
         EDGetTokenT<View<reco::GenParticle> > genParticleToken_;
         EDGetTokenT<int> stage0catToken_, stage1catToken_, njetsToken_;
+        EDGetTokenT<HTXS::HiggsClassification> newHTXSToken_;
         EDGetTokenT<float> pTHToken_,pTVToken_;
         string systLabel_;
         bool requireScaledPtCuts_;
@@ -62,6 +64,8 @@ namespace flashgg {
         njetsToken_ = consumes<int>( HTXSps.getParameter<InputTag>("njets") );
         pTHToken_ = consumes<float>( HTXSps.getParameter<InputTag>("pTH") );
         pTVToken_ = consumes<float>( HTXSps.getParameter<InputTag>("pTV") );
+        newHTXSToken_ = consumes<HTXS::HiggsClassification>( HTXSps.getParameter<InputTag>("ClassificationObj") );
+
 
         produces<vector<UntaggedTag> >();
         produces<vector<TagTruthBase> >();
@@ -86,6 +90,9 @@ namespace flashgg {
         evt.getByToken(njetsToken_,njets);
         evt.getByToken(pTHToken_,pTH);
         evt.getByToken(pTVToken_,pTV);
+        Handle<HTXS::HiggsClassification> htxsClassification;
+        evt.getByToken(newHTXSToken_,htxsClassification);
+
 
         Handle<View<flashgg::DiPhotonCandidate> > diPhotons;
         evt.getByToken( diPhotonToken_, diPhotons );
@@ -154,6 +161,13 @@ namespace flashgg {
                                                *( njets.product() ),
                                                *( pTH.product() ),
                                                *( pTV.product() ) );
+                    } else if ( htxsClassification.isValid() ) {
+                        truth_obj.setHTXSInfo( htxsClassification->stage0_cat,
+                                               htxsClassification->stage1_cat_pTjet30GeV,
+                                               htxsClassification->jets30.size(),
+                                               htxsClassification->p4decay_higgs.pt(),
+                                               htxsClassification->p4decay_V.pt() );
+
                     } else {
                         truth_obj.setHTXSInfo( 0, 0, 0, 0., 0. );
                     }
