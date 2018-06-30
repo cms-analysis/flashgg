efa9a7d3b5508876077552aa77d71b1017e504c3
Seth Zenz <seth.zenz@cern.ch>
2018-06-30 18:41:25 +0200

    1047 submission update

--- Systematics/test/tth_sig_jobs_2017.sh
+++ Systematics/test/tth_sig_jobs_2017.sh
@@ -2,5 +2,5 @@
 #outdir="/afs/cern.ch/work/s/sethzenz/ws/" # can't set absolute path on lsf because we're expecting to stage
 queue="2nd"
 useAAA=0
-version="1046"
+version="1047"
 fggRunJobs.py --load tth_sig_jobs_2017.json -d tth_sig_jobs_$version -x cmsRun workspaceStd.py maxEvents=-1 -n 500 -q $queue -D -P useAAA=$useAAA doHTXS=True doFiducial=False tthTagsOnly=True 
