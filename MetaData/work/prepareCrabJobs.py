7a674c6e194f3d0e8d3b0142611f28f8953d02d7
andreh7 <andre.holzner@gmail.com>
2018-07-18 14:04:02 +0200

    simplified code to abbreviate job name to at most 97 characters (#1059)

--- MetaData/work/prepareCrabJobs.py
+++ MetaData/work/prepareCrabJobs.py
@@ -266,81 +266,56 @@ if options.createCrabConfig:
         while os.path.isdir("crab_" + jobname):
             itry += 1
             jobname = "_".join([flashggVersion, PrimaryDataset, ProcessedDataset, str(itry).zfill(2)])
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISummer16MiniAODv2-PUMoriond17_qcut19_80X_mcRun2_asymptotic_2016","Moriond17")            
-        if len(jobname) > 97:
-            jobname = jobname.replace("TuneCUEP8M1_13TeV-pythia8","13TeV")
-        if len(jobname) > 97:
-            jobname = jobname.replace("TuneCUETP8M1_13TeV-madgraphMLM-pythia8","13TeV-mg")
-        if len(jobname) > 97:
-            jobname = jobname.replace("TuneCUETP8M1_13TeV_Pythia8","13TeV-p8")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RSGravToGG","Grav")
-        if len(jobname) > 97:
-            jobname = jobname.replace("-PU25nsData2015v1","")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall15MiniAODv2_","")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall15MiniAODv2-magnetOffBS0T_PU25nsData2015v1_0T","0T")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISpring16MiniAODv1-PUSpring16_80X_mcRun2","Spring16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2","Spring16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISpring16MiniAODv1-PUSpring16RAWAODSIM_80X_mcRun2","Spring16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISpring16MiniAODv2-PUSpring16RAWAODSIM","Spring16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISpring16MiniAODv1-PUSpring16RAWAODSIM","Spring16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("plus","p")
-        if len(jobname) > 97:
-            jobname = jobname.replace("minus","m")
-        if len(jobname) > 97:
-            jobname = jobname.replace("percentMaterial","Mat")
-        if len(jobname) > 97:
-            jobname = jobname.replace("TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8","13TeV-amcnlo-p8")
-        if len(jobname) > 97:
-            jobname = jobname.replace("80X_mcRun2_asymptotic_2016_miniAODv2","asym16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("pythia8","p8")
-        if len(jobname) > 97:
-            jobname = jobname.replace("mcRun2_asymptotic_2016","asym16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("asymptotic_2016","asym16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("reHLT_80X_mcRun2_asymptotic_v14-v1","reHLTasym16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("_withHLT_80X_mcRun2_asymptotic_v14_ext1-v1","reHLTasym16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISummer16MiniAODv2-PUMoriond17","Summer16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("TrancheIV","T4")
-        if len(jobname) > 97:
-            jobname = jobname.replace("PhaseISpring17MiniAOD-FlatPU28to62_90X_upgrade2017_realistic_v20-v1","PhaseISpring17")
-        if len(jobname) > 97:
-            jobname = jobname.replace("PhaseISpring17MiniAOD-FlatPU28to62_902_90X_upgrade2017_realistic_v20_ext1-v1","PhaseISpring17")
-        if len(jobname) > 97:
-            jobname = jobname.replace("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16")
-            jobname = jobname.replace("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_HIG066_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16")
-            jobname = jobname.replace("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_HIG067_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16")
-            jobname = jobname.replace("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_HIG068_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1","94Xv10")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAOD-94X_mc2017_realistic_v10","94Xv10")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAOD-94X_mc2017_realistic_v11","94Xv11")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAOD-PU2017_94X_mc2017_realistic_v11","PU2017_94Xv11")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAOD-1core_94X_mc2017_realistic_v10","1core_94Xv10")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIISummer17MiniAOD-NZSFlatPU28to62_92X_upgrade2017_realistic_v10","FlatPU_92Xv10")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14","PU2017_94Xv14")
-        if len(jobname) > 97:
-            jobname = jobname.replace("RunIIFall17MiniAODv2-PU2017_12Apr2018_1core_94X_mc2017_realistic_v14","1core_94Xv1")
+
+
+        # list of (original, replacement) to bring dataset name to 97 characters or less
+        replacements = [
+            ("RunIISummer16MiniAODv2-PUMoriond17_qcut19_80X_mcRun2_asymptotic_2016","Moriond17"),
+            ("TuneCUEP8M1_13TeV-pythia8","13TeV"),
+            ("TuneCUETP8M1_13TeV-madgraphMLM-pythia8","13TeV-mg"),
+            ("TuneCUETP8M1_13TeV_Pythia8","13TeV-p8"),
+            ("RSGravToGG","Grav"),
+            ("-PU25nsData2015v1",""),
+            ("RunIIFall15MiniAODv2_",""),
+            ("RunIIFall15MiniAODv2-magnetOffBS0T_PU25nsData2015v1_0T","0T"),
+            ("RunIISpring16MiniAODv1-PUSpring16_80X_mcRun2","Spring16"),
+            ("RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2","Spring16"),
+            ("RunIISpring16MiniAODv1-PUSpring16RAWAODSIM_80X_mcRun2","Spring16"),
+            ("RunIISpring16MiniAODv2-PUSpring16RAWAODSIM","Spring16"),
+            ("RunIISpring16MiniAODv1-PUSpring16RAWAODSIM","Spring16"),
+            ("plus","p"),
+            ("minus","m"),
+            ("percentMaterial","Mat"),
+            ("TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8","13TeV-amcnlo-p8"),
+            ("80X_mcRun2_asymptotic_2016_miniAODv2","asym16"),
+            ("pythia8","p8"),
+            ("mcRun2_asymptotic_2016","asym16"),
+            ("asymptotic_2016","asym16"),
+            ("reHLT_80X_mcRun2_asymptotic_v14-v1","reHLTasym16"),
+            ("_withHLT_80X_mcRun2_asymptotic_v14_ext1-v1","reHLTasym16"),
+            ("RunIISummer16MiniAODv2-PUMoriond17","Summer16"),
+            ("TrancheIV","T4"),
+            ("PhaseISpring17MiniAOD-FlatPU28to62_90X_upgrade2017_realistic_v20-v1","PhaseISpring17"),
+            ("PhaseISpring17MiniAOD-FlatPU28to62_902_90X_upgrade2017_realistic_v20_ext1-v1","PhaseISpring17"),
+            ("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16"),
+            ("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_HIG066_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16"),
+            ("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_HIG067_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16"),
+            ("PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_HIG068_90X_upgrade2017_realistic_v6_C1-v1","PhaseIFall16"),
+            ("RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1","94Xv10"),
+            ("RunIIFall17MiniAOD-94X_mc2017_realistic_v10","94Xv10"),
+            ("RunIIFall17MiniAOD-94X_mc2017_realistic_v11","94Xv11"),
+            ("RunIIFall17MiniAOD-PU2017_94X_mc2017_realistic_v11","PU2017_94Xv11"),
+            ("RunIIFall17MiniAOD-1core_94X_mc2017_realistic_v10","1core_94Xv10"),
+            ("RunIISummer17MiniAOD-NZSFlatPU28to62_92X_upgrade2017_realistic_v10","FlatPU_92Xv10"),
+            ("RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14","PU2017_94Xv14"),
+            ("RunIIFall17MiniAODv2-PU2017_12Apr2018_1core_94X_mc2017_realistic_v14","1core_94Xv1"),
+            ]
+
+        for orig, replacement in replacements:
+            if len(jobname) <= 97:
+                break
+            jobname = jobname.replace(orig, replacement)
+
         if len(jobname) > 97:
             print orig_jobname
             print "-->", len(jobname), jobname
