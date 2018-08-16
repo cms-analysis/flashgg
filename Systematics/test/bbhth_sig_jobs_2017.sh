51a050dec8f31d293a824d34ea00f9e835ff0cb3
Seth Zenz <sethzenz@gmail.com>
2018-08-16 13:12:19 +0200

    catalogue 3_1_1: new samples + STXS bugfix for VBF samples (#1069)
    * trivial script update for 1058 ws
    * catalogue 3_1_1: new samples + STXS bugfix for VBF samples

--- Systematics/test/bbhth_sig_jobs_2017.sh
+++ Systematics/test/bbhth_sig_jobs_2017.sh
@@ -2,6 +2,6 @@
 #outdir="/afs/cern.ch/work/s/sethzenz/ws/" # can't set absolute path on lsf because we're expecting to stage
 queue="2nd"
 useAAA=0
-version="1047"
+version="1058"
 # doHTXS is false but rapidity filter will be applied
 fggRunJobs.py --load bbhth_sig_jobs_2017.json -d bbhth_sig_jobs_$version -x cmsRun workspaceStd.py maxEvents=-1 -n 500 -q $queue -D -P useAAA=$useAAA doHTXS=False doFiducial=False tthTagsOnly=True
