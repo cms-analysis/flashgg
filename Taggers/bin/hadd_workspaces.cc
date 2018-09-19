fb38b75c547bad40008c41862adba6852a1eba9e
Nadezda <nadezda.chernyavskaya@gmail.com>
2018-09-19 09:23:19 +0200

    hadd_workspaces adding also trees to workspaces files

--- Taggers/bin/hadd_workspaces.cc
+++ Taggers/bin/hadd_workspaces.cc
@@ -10,7 +10,7 @@
 
 int main( int argc, char *argv[] )
 {
-    bool doTreesAndHistograms = false;
+    bool doTreesAndHistograms = true;
 
     std::vector<string> input;
     std::string outputfile = argv[1];
