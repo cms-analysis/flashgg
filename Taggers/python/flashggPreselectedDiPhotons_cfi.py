140940b06aa4a197ec0d10152c3785635309f136
Andrea Beschi <a.beschi@campus.unimib.it>
2018-09-25 13:05:35 +0200

    Updated ttH leptonic BDT with high stat diphoton sample & updated preselection pT thresholds to 2017 values

--- Taggers/python/flashggPreselectedDiPhotons_cfi.py
+++ Taggers/python/flashggPreselectedDiPhotons_cfi.py
@@ -69,7 +69,7 @@ flashggPreselectedDiPhotons = cms.EDFilter(
         "    (leadingPhoton.full5x5_r9>0.8||leadingPhoton.egChargedHadronIso<20||leadingPhoton.egChargedHadronIso/leadingPhoton.pt<0.3)"
         " && (subLeadingPhoton.full5x5_r9>0.8||subLeadingPhoton.egChargedHadronIso<20||subLeadingPhoton.egChargedHadronIso/subLeadingPhoton.pt<0.3)"
         " && (leadingPhoton.hadronicOverEm < 0.08 && subLeadingPhoton.hadronicOverEm < 0.08)"
-        " && (leadingPhoton.pt >30.0 && subLeadingPhoton.pt > 20.0)"
+        " && (leadingPhoton.pt >35.0 && subLeadingPhoton.pt > 25.0)"
         " && (abs(leadingPhoton.superCluster.eta) < 2.5 && abs(subLeadingPhoton.superCluster.eta) < 2.5)"
         " && (abs(leadingPhoton.superCluster.eta) < 1.4442 || abs(leadingPhoton.superCluster.eta) > 1.566)"
         " && (abs(subLeadingPhoton.superCluster.eta) < 1.4442 || abs(subLeadingPhoton.superCluster.eta) > 1.566)"
