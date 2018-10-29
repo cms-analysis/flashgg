7c6cf55ca19aad3bb1545b4f3680366079a60bb5
Simone Pigazzini <simone.pigazzini@cern.ch>
2018-10-29 16:58:53 +0100

    Add Met Pt in TTHLeptonic dataformat

--- DataFormats/interface/TTHLeptonicTag.h
+++ DataFormats/interface/TTHLeptonicTag.h
@@ -29,16 +29,19 @@ namespace flashgg {
         const std::vector<double>  leptonsPhi() const { return lepPhi_;}
         const std::vector<double>  leptonsEta() const { return lepEta_;}
         const std::vector<int>  leptonsType() const { return lepType_;}
+
+        const float& metPt() const { return metPt_; }
         
         void setJets( std::vector<edm::Ptr<Jet> > Jets ) { Jets_ = Jets; }
         void setBJets( std::vector<edm::Ptr<Jet> > BJets )  { BJets_ = BJets;}
         void setMuons( std::vector<edm::Ptr<Muon> > Muons ) {Muons_ = Muons;}
         void setElectrons( std::vector<edm::Ptr<Electron> > Electrons ) {Electrons_ = Electrons;}
-        void setLepPt( std::vector<double> lepPt) { lepPt_ = lepPt; }
-        void setLepE( std::vector<double> lepE) { lepE_ = lepE; }
-        void setLepEta( std::vector<double> lepEta) { lepEta_ = lepEta; }
-        void setLepPhi( std::vector<double> lepPhi) { lepPhi_ = lepPhi; }
-        void setLepType( std::vector<int> lepType) { lepType_ = lepType; }
+        void setLepPt( std::vector<double> lepPt ) { lepPt_ = lepPt; }
+        void setLepE( std::vector<double> lepE ) { lepE_ = lepE; }
+        void setLepEta( std::vector<double> lepEta ) { lepEta_ = lepEta; }
+        void setLepPhi( std::vector<double> lepPhi ) { lepPhi_ = lepPhi; }
+        void setLepType( std::vector<int> lepType ) { lepType_ = lepType; }
+        void setMetPt( float& metPt ) { metPt_ = metPt; }
 
         DiPhotonTagBase::tag_t tagEnum() const override {return DiPhotonTagBase::kTTHLeptonic; }
 
@@ -57,6 +60,8 @@ namespace flashgg {
         std::vector<double> lepPhi_;
         std::vector<int>    lepType_;
 
+        float metPt_;
+        
         float mvaRes_;
     };
 }
