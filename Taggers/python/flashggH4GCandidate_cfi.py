144df5451dee403a5ef3ca59d59eb4c1bb54581d
wamorkart <wamorkar.t@husky.neu.edu>
2018-11-15 00:43:51 +0100

    Minor fix

--- Taggers/python/flashggH4GCandidate_cfi.py
+++ Taggers/python/flashggH4GCandidate_cfi.py
@@ -1,5 +1,11 @@
 import FWCore.ParameterSet.Config as cms
-
+from h4gCandidateDumper_cfi import h4gCandidateDumper
 from flashggH4GTag_cfi import flashggH4GTag
 
+
+FlashggH4GCandidate = cms.EDProducer("FlashggH4GCandidateProducer",
+                                     PhotonTag              = cms.InputTag('flashggRandomizedPhotons'),
+                                     DiPhotonTag            = cms.InputTag('flashggDiPhotons'),
+                                     VertexTag              = cms.InputTag('offlineSlimmedPrimaryVertices')
+                                     )
 flashggH4GTagSequence = cms.Sequence( flashggH4GTag )
