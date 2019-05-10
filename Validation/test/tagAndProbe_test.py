65c0848404b978ddcc6bcc8e0a0f9f32e8253b97
Thomas Reitenspiess <thomas.reitenspiess@cern.ch>
2019-05-10 16:48:08 +0200

    Working configuration for tNp ntutples. Scales and smearings not working

--- Validation/test/tagAndProbe_test.py
+++ Validation/test/tagAndProbe_test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env cmsRun
+import importlib
 
 import FWCore.ParameterSet.Config as cms
 import FWCore.Utilities.FileUtils as FileUtils
 import FWCore.ParameterSet.VarParsing as VarParsing
+from flashgg.MetaData.MetaConditionsReader import *
+
 
 process = cms.Process("Analysis")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
@@ -20,68 +23,109 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-from Configuration.AlCa.GlobalTag import GlobalTag
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
 
 from flashgg.MetaData.JobConfig import customize
-
-customize.options.register ('trigger',
+customize.options.register ("trigger",
                             "", # default value
                             VarParsing.VarParsing.multiplicity.singleton, # singleton or list
                             VarParsing.VarParsing.varType.string,          # string, int, or float
                             "trigger")
 
+
 customize.setDefault("maxEvents",10000)
 customize.setDefault("targetLumi",1.e+3)
+customize.parse()
+customize.metaConditions = MetaConditionsReader(customize.metaConditions)
 
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+if customize.processId == "Data":
+    process.GlobalTag.globaltag = str(customize.metaConditions['globalTags']['data'])
+else:
+    process.GlobalTag.globaltag = str(customize.metaConditions['globalTags']['MC'])
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
+
+process.load("flashgg.MicroAOD.flashggDiPhotons_cfi")
 process.load("flashgg.Validation.FlashggTagAndProbeProducer_cfi")
-process.load("flashgg.Validation.tagAndProbeDumper_cfi")    
+process.load("flashgg.Validation.tagAndProbeDumper_cfi")  
+process.load("flashgg.Systematics.flashggDiPhotonSystematics_cfi")
+process.load("flashgg.Taggers.flashggUpdatedIdMVADiPhotons_cfi")
 
 from flashgg.Validation.FlashggTagAndProbeProducer_cfi import flashggTagAndProbe
 
 import flashgg.Taggers.dumperConfigTools as cfgTools
 import flashgg.Validation.tagAndProbeDumperConfig as dumpCfg
 
+from flashgg.Systematics.flashggDiPhotonSystematics_cfi import flashggDiPhotonSystematics
+
 variables = dumpCfg.getDefaultConfig()
 dumpCfg.addRegressionInput(variables)
-
-
-eleSource="flashggSelectedElectrons"
-
-from flashgg.MicroAOD.flashggLeptonSelectors_cff import flashggSelectedElectrons
-process.flashggIdentifiedElectrons = flashggSelectedElectrons.clone( 
-    src=cms.InputTag(eleSource),
-    cut=cms.string("passTightId")
-)
-
-variables.extend( ["leadEleMatch    := leadingPhoton.hasUserCand('eleMatch')",
-                       "subleadEleMatch := subLeadingPhoton.hasUserCand('eleMatch')"
-                   ] )
-    # store cut-based IDs
-for eid in "Loose", "Medium", "Tight":
-    variables.extend( [
-        "leadEleIs%s    := ?leadingPhoton.hasUserCand('eleMatch')?leadingPhoton.userCand('eleMatch').pass%sId:0"       % (eid,eid),
-        "subleadEleIs%s := ?subLeadingPhoton.hasUserCand('eleMatch')?subLeadingPhoton.userCand('eleMatch').pass%sId:0" % (eid,eid)
-    ])
 
 matchTriggerPaths = []
 dataTriggers = customize.options.trigger.split(",")
 strippedNames = list(map(lambda x: x.rstrip("*"), dataTriggers))
 matchTriggerPaths = "&& ".join( map(lambda x: "userInt('%s')" % x, strippedNames) )
 print(matchTriggerPaths)
+dumpBits = list(strippedNames)
+
+from flashgg.MicroAOD.flashggLeptonSelectors_cff import flashggSelectedElectrons
+process.flashggIdentifiedElectrons = flashggSelectedElectrons.clone( 
+    src=cms.InputTag("flashggSelectedElectrons"),
+    cut=cms.string("passTightId")
+)
+
+
+systModules = cms.VPSet(
+        cms.PSet( PhotonMethodName = cms.string("FlashggPhotonHLTMatch"),
+                  MethodName = cms.string("FlashggDiPhotonFromPhoton"),
+                  Label = cms.string("hltMatch"),
+                  NSigmas = cms.vint32(),
+                  ApplyCentralValue = cms.bool(True),
+                  trgBitsSrc = cms.InputTag("TriggerResults","","HLT"),
+                  trgObjectsSrc = cms.InputTag("slimmedPatTrigger"),
+                  pathNames = cms.vstring(dumpBits),
+                  deltaRmax = cms.double(0.3),
+                  ),
+        cms.PSet( PhotonMethodName = cms.string("FlashggPhotonEleMatch"),
+                  MethodName = cms.string("FlashggDiPhotonFromPhoton"),
+                  Label = cms.string("eleMatch"),
+                  NSigmas = cms.vint32(),
+                  ApplyCentralValue = cms.bool(True),
+                  electronsSrc = cms.InputTag("flashggIdentifiedElectrons"),
+                  )
+)
+variables.extend( ["leadEleMatch    := leadingPhoton.hasUserCand('eleMatch')",
+                   "subleadEleMatch := subLeadingPhoton.hasUserCand('eleMatch')"
+               ] )
+
+# Only EGM tools working atm. photonSmearBins and photonScaleUncertBins missing in flashggDiPhotonSystematics2018_cfi.py
+# EGM not working due to missing NSigmas somewhere
+sysmodule = importlib.import_module("flashgg.Systematics."+customize.metaConditions["flashggDiPhotonSystematics"])
+systModules.append(sysmodule.MCScaleHighR9EB_EGM)
+systModules.append(sysmodule.MCScaleLowR9EB_EGM)
+systModules.append(sysmodule.MCScaleHighR9EE_EGM)
+systModules.append(sysmodule.MCScaleLowR9EE_EGM)
+systModules.append(sysmodule.MCScaleGain6EB_EGM)
+systModules.append(sysmodule.MCScaleGain1EB_EGM)
+systModules.append(sysmodule.MCSmearHighR9EE_EGM)
+systModules.append(sysmodule.MCSmearLowR9EE_EGM)
+systModules.append(sysmodule.MCSmearHighR9EB_EGM)
+systModules.append(sysmodule.MCSmearLowR9EB_EGM)
+
+process.flashggDiPhotonSystematics=flashggDiPhotonSystematics
+process.flashggDiPhotonSystematics.SystMethods = systModules
 
 process.flashggTagAndProbe = flashggTagAndProbe
-process.flashggTagAndProbe.diphotonsSrc = "kinDiPhotons"
-process.flashggTagAndProbe.tagSelection = "%s && pt > 40 && (?hasUserCand('eleMatch')?userCand('eleMatch').passTightId:0) && hasPixelSeed && egChargedHadronIso < 20 && egChargedHadronIso/pt < 0.3" % matchTriggerPaths 
-process.flashggTagAndProbe.probeSelection = "hasPixelSeed && egChargedHadronIso < 20 && egChargedHadronIso/pt < 0.3"
-# process.flashggTagAndProbe.idSelection = cms.PSet(
-#     rho = highMassCiCPhotonsV2.rho,
-#     cut = highMassCiCPhotonsV2.cut,        
-#     variables = highMassCiCPhotonsV2.variables,
-#     categories = highMassCiCPhotonsV2.categories
-# ) #This only adds flags to the ntuples to test a particular selection, does not affect the tag or probe selection
+process.flashggTagAndProbe.diphotonsSrc = cms.InputTag("flashggDiPhotonSystematics")
+process.flashggTagAndProbe.tagSelection = "{} && pt > 40 && (?hasUserCand('eleMatch')?userCand('eleMatch').passTightId:0) && hasPixelSeed && egChargedHadronIso < 20 && egChargedHadronIso/pt < 0.3".format(matchTriggerPaths)
+process.flashggTagAndProbe.probeSelection = "egChargedHadronIso < 20 && egChargedHadronIso/pt < 0.3"
+process.flashggTagAndProbe.idSelection = cms.PSet( 
+    rho = cms.InputTag("fixedGridRhoAll"),
+    cut = cms.string("(full5x5_r9>0.8||egChargedHadronIso<20||egChargedHadronIso/pt<0.3) && egChargedHadronIso < 15"),
+    variables = cms.vstring(),
+    categories = cms.VPSet( cms.PSet(cut=cms.string("1"), selection=cms.VPSet( cms.VPSet() )) ) )
 
 from flashgg.Validation.tagAndProbeDumper_cfi import tagAndProbeDumper
 tagAndProbeDumper.dumpTrees = True
@@ -98,6 +142,6 @@ cfgTools.addCategories(tagAndProbeDumper,
                        histograms=[]
                    )    
 tnp_sequence = cms.Sequence(flashggTagAndProbe+tagAndProbeDumper)
-process.p = cms.Path(tnp_sequence)
+process.p = cms.Path(process.flashggUpdatedIdMVADiPhotons*process.flashggIdentifiedElectrons*process.flashggDiPhotonSystematics*tnp_sequence)
 
 customize(process)
