7d9a3b72ef7d6db8614b83b566329c57d3181612
simonepigazzini <simonepigazzini@users.noreply.github.com>
2019-08-04 23:23:24 +0200

    Legacy RunII V1 production p0 (#1128)
    * Legacy RunII V1 production p0
    
    * Removing unnecessary tHq flag
    
    * Improved sample listing

--- MicroAOD/python/MicroAODCustomize.py
+++ MicroAOD/python/MicroAODCustomize.py
@@ -134,7 +134,7 @@ class MicroAODCustomize(object):
             self.customizeSignal(process)
             if "tth" in customize.datasetName.lower():
                 self.customizeTTH(process)
-            elif "vh" in customize.datasetName.lower():
+            elif "vh" in customize.datasetName.lower() or "wmh" in customize.datasetName.lower() or "wph" in customize.datasetName.lower() or "wh" in customize.datasetName.lower() or "zh" in customize.datasetName.lower():
                 self.customizeVH(process)
             elif "ggh" in customize.datasetName.lower() or "glugluh" in customize.datasetName.lower():
                 self.customizeGGH(process)
@@ -559,8 +559,8 @@ class MicroAODCustomize(object):
         # process.rivetProducerHTXS.ProductionMode = "TH"
 #        process.flashggPDFWeightObject.LHEEventTag = "source"
 #        process.flashggPDFWeightObject.LHERunLabel = "source"
-        process.flashggPDFWeightObject.isStandardSample = False
-        process.flashggPDFWeightObject.isThqSample = True
+        # process.flashggPDFWeightObject.isStandardSample = False
+        # process.flashggPDFWeightObject.isThqSample = True
 
     def customizePhotons(self, process):
         for opt, value in self.metaConditions["flashggPhotons"].items():
