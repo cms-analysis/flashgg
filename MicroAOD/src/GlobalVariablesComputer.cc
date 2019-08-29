8f266b92b4b8c622b0f912c104ccb4cea972a3a6
simonepigazzini <simonepigazzini@users.noreply.github.com>
2019-08-29 13:24:29 +0200

    Load parent and secondary dataset files in JobConfig  (#1141)
    * Test parent dset mode with Rivet producer
    
    * Add secondaryDataset option in JobConfig

--- MicroAOD/src/GlobalVariablesComputer.cc
+++ MicroAOD/src/GlobalVariablesComputer.cc
@@ -71,7 +71,12 @@ namespace flashgg {
                 puWeight_ = cfg.getParameter<std::vector<double> >("dataPu");
                 auto mcpu = cfg.getParameter<std::vector<double> >("mcPu");
 
-                assert( puWeight_.size() == mcpu.size() );
+                if( puWeight_.size() != mcpu.size() )
+                {
+                    std::ostringstream msg;
+                    msg << "PU weight size: " << puWeight_.size() << " \n MC PU size: " << mcpu.size();
+                    throw std::logic_error(msg.str());
+                }
                 if ( puWeight_.size() != puBins_.size()-1 ) {
                     puBins_.resize(puWeight_.size()+1);
                     for (unsigned int i=0; i<puBins_.size(); ++i)
