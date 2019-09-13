e2b7f9825245a522d648d8c111aa7073f7c33d47
Julie Malcles <julie.malcles@gmail.com>
2019-09-13 23:24:23 +0200

    first changes for muon SF still to be tested

--- Systematics/python/SystematicsCustomize.py
+++ Systematics/python/SystematicsCustomize.py
@@ -77,7 +77,7 @@ def createStandardSystematicsProducers(process, options):
     diPhotons_syst.setupDiPhotonSystematics( process, options )
 
     import flashgg.Systematics.flashggMuonSystematics_cfi as muon_sf
-    muon_sf.SetupMuonScaleFactors( process , options.metaConditions["MUON_ID"], options.metaConditions["MUON_ISO"] )
+    muon_sf.SetupMuonScaleFactors( process ,  options.metaConditions["MUON_ID_JSON_FileName"],  options.metaConditions["MUON_ID_JSON_FileName_LowPt"], options.metaConditions["MUON_ISO_JSON_FileName"], options.metaConditions["MUON_ID"], options.metaConditions["MUON_ISO"], options.metaConditions["MUON_ID_RefTracks"],options.metaConditions["MUON_ID_RefTracks_LowPt"] )
    
     #scale factors for electron ID
     from   flashgg.Systematics.flashggElectronSystematics_cfi import EleSF_JSONReader
--- Systematics/python/flashggMuonSystematics_cfi.py
+++ Systematics/python/flashggMuonSystematics_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-import os, json, re
+import os, json, re, math
+
 class MuonSF_JSONReader :
-    def __init__(self , file_name , sf_name ):
+    def __init__(self , file_name , sf_name , lowPt_file_name="" , lowPt_sf_name="", extendPt=0 ):
         self.Name = sf_name
         self.JSONFileName = os.path.expanduser( os.path.expandvars(file_name) )
         with open( self.JSONFileName , 'r' ) as f :
@@ -12,8 +13,34 @@ class MuonSF_JSONReader :
             variables = cms.vstring( "eta" , "pt" ),
             bins = cms.VPSet() 
             )
-        format_bins = r'^(?P<VarName>.*):\[(?P<From>\d*.\d*),(?P<To>\d*.\d*)\]'
+
         sub_branch_name = self.Info.keys()[0]
+
+        #First loop to get min pT
+        format_bins = r'^(?P<VarName>.*):\[(?P<From>\d*.\d*),(?P<To>\d*.\d*)\]'
+
+        minPt = 1000
+        for eta_region in self.Info[sub_branch_name] :
+            for pt_region in self.Info[sub_branch_name][eta_region] :
+                pt_values = re.match( format_bins , pt_region , re.M|re.I )
+                if pt_values and len( pt_values.groups() ) == 3 :
+                    pt_from = float( pt_values.group("From" ) )
+                    if pt_from < minPt :
+                        minPt = pt_from
+
+        if lowPt_file_name!="" and sf_name!="NUM_HighPtID_DEN_genTracks" and sf_name!="NUM_MediumPromptID_DEN_genTracks" and sf_name!="NUM_TrkHighPtID_DEN_genTracks" :
+            self.JSONFileNameForLowPt = os.path.expanduser( os.path.expandvars(lowPt_file_name) )
+            with open( self.JSONFileNameForLowPt , 'r' ) as f_lowPt :
+                j_lowPt = json.load( f_lowPt ) 
+                self.Info_lowPt = j_lowPt[ sf_name ]
+                for eta_region in self.Info_lowPt[sub_branch_name] :
+                    for pt_region in self.Info_lowPt[sub_branch_name][eta_region] :
+                        pt_values = re.match( format_bins , pt_region , re.M|re.I )
+                        if pt_values and len( pt_values.groups() ) == 3 :
+                            pt_to = float( pt_values.group("To" ) )
+                            if pt_to <= minPt:
+                                self.Info[sub_branch_name][eta_region][pt_region] = self.Info_lowPt[sub_branch_name][eta_region][pt_region]
+
         for eta_region in self.Info[sub_branch_name] :
             eta_values = re.match( format_bins , eta_region , re.M|re.I )
             if eta_values and len( eta_values.groups() ) == 3 :
@@ -21,15 +48,21 @@ class MuonSF_JSONReader :
                 eta_to = float( eta_values.group("To") )
 
                 pt_bins = {}
+                minPtBin = 0
+
                 for pt_region in self.Info[sub_branch_name][eta_region] :
+                    if pt_region.count("[" + str(minPt)) :
+                        minPtBin = pt_region
                     pt_values = re.match( format_bins , pt_region , re.M|re.I )
                     if pt_values and len( pt_values.groups() ) == 3 :
                         pt_from = float( pt_values.group("From" ) )
                         pt_to = float( pt_values.group("To" ) )
 
                         sf_value = self.Info[sub_branch_name][eta_region][pt_region]["value"]
-                        sf_error = self.Info[sub_branch_name][eta_region][pt_region]["error"]
-                        
+                        sf_stat = self.Info[sub_branch_name][eta_region][pt_region]["stat"]
+                        sf_syst = self.Info[sub_branch_name][eta_region][pt_region]["syst"]
+
+                        sf_error = math.sqrt(sf_stat*sf_stat + sf_syst*sf_syst)
                         self.binInfo.bins.append( 
                             cms.PSet(
                                 lowBounds = cms.vdouble( eta_from , pt_from ) ,
@@ -51,22 +84,64 @@ class MuonSF_JSONReader :
                     else :
                         print pt_region, "of sf" , sf_name , "from file", file_name , "can not be interpreted correctly"
                 min_pt = sorted( pt_bins.keys() )[0]
-                self.binInfo.bins.append( 
-                    cms.PSet(
-                        lowBounds = cms.vdouble( eta_from , 0.00 ) ,
-                        upBounds = cms.vdouble( eta_to , min_pt ) ,
-                        values = cms.vdouble( 1.00 ) ,
-                        uncertainties = cms.vdouble( 0.00 , 0.00 )
+
+                if extendPt!=0 and min_pt>extendPt: #create one bin from extendPt to min_pt with next bin SF and doubled uncertainty
+
+                    minSF = self.Info[sub_branch_name][eta_region][minPtBin]["value"]
+                    minStat = self.Info[sub_branch_name][eta_region][minPtBin]["stat"]
+                    minSyst = self.Info[sub_branch_name][eta_region][minPtBin]["syst"]
+                    minErr = math.sqrt(minSyst*minSyst +minStat*minStat )
+
+                    self.binInfo.bins.append( 
+                        cms.PSet(
+                            lowBounds = cms.vdouble( eta_from , 0.00 ) ,
+                            upBounds = cms.vdouble( eta_to , extendPt ) ,
+                            values = cms.vdouble( 1.00 ) ,
+                            uncertainties = cms.vdouble( 0.00 , 0.00 )
+                            )
                         )
-                    )
-                self.binInfo.bins.append( 
-                    cms.PSet(
-                        lowBounds = cms.vdouble( -1*eta_to , 0.00 ) ,
-                        upBounds = cms.vdouble( -1*eta_from , min_pt ) ,
-                        values = cms.vdouble( 1.00 ) ,
-                        uncertainties = cms.vdouble( 0.00 , 0.00 )
+                    self.binInfo.bins.append( 
+                        cms.PSet(
+                            lowBounds = cms.vdouble( -1*eta_to , 0.00 ) ,
+                            upBounds = cms.vdouble( -1*eta_from , extendPt ) ,
+                            values = cms.vdouble( 1.00 ) ,
+                            uncertainties = cms.vdouble( 0.00 , 0.00 )
+                            )
                         )
-                    )
+                    self.binInfo.bins.append( 
+                        cms.PSet(
+                            lowBounds = cms.vdouble( eta_from , extendPt ) ,
+                            upBounds = cms.vdouble( eta_to , min_pt ) ,
+                            values = cms.vdouble( minSF ) ,
+                            uncertainties = cms.vdouble( 2*minErr , 2*minErr )
+                            )
+                        )
+                    self.binInfo.bins.append( 
+                        cms.PSet(
+                            lowBounds = cms.vdouble( -1*eta_to , extendPt ) ,
+                            upBounds = cms.vdouble( -1*eta_from , min_pt ) ,
+                            values = cms.vdouble( minSF ) ,
+                            uncertainties = cms.vdouble( 2*minErr , 2*minErr )
+                            )
+                        )
+                else :
+                    self.binInfo.bins.append( 
+                        cms.PSet(
+                            lowBounds = cms.vdouble( eta_from , 0.00 ) ,
+                            upBounds = cms.vdouble( eta_to , min_pt ) ,
+                            values = cms.vdouble( 1.00 ) ,
+                            uncertainties = cms.vdouble( 0.00 , 0.00 )
+                            )
+                        )
+                    self.binInfo.bins.append( 
+                        cms.PSet(
+                            lowBounds = cms.vdouble( -1*eta_to , 0.00 ) ,
+                            upBounds = cms.vdouble( -1*eta_from , min_pt ) ,
+                            values = cms.vdouble( 1.00 ) ,
+                            uncertainties = cms.vdouble( 0.00 , 0.00 )
+                            )
+                        )
+
                 pt_bins[ sorted(pt_bins.keys())[-1] ][0].upBounds[1] = float( 'inf' )
                 pt_bins[ sorted(pt_bins.keys())[-1] ][1].upBounds[1] = float( 'inf' )
             else:
@@ -85,14 +160,6 @@ class MuonSF_JSONReader :
                                          )
         return self.SystematicsPSET
 
-MUON_ID_JSON_FileName = "$CMSSW_BASE/src/flashgg/Systematics/data/Muon_RunBCDEF_SF_ID.json"
-MUON_ID_ScaleFactors = {}
-for mu_id in ["Tight", "Medium" , "Loose", "Soft", "HighPt", "MediumPrompt", "TrkHighPt"] :
-    MUON_ID_ScaleFactors[mu_id] = MuonSF_JSONReader( MUON_ID_JSON_FileName ,  "NUM_%sID_DEN_genTracks"%mu_id )
-MUON_ISO_JSON_FileName = "$CMSSW_BASE/src/flashgg/Systematics/data/Muon_RunBCDEF_SF_ISO.json"
-MUON_ISO_ScaleFactors = {}
-for iso in ["LooseRelIso_DEN_LooseID", "LooseRelIso_DEN_MediumID", "TightRelIso_DEN_MediumID", "LooseRelTkIso_DEN_TrkHighPtID", "TightRelTkIso_DEN_TrkHighPtID", "LooseRelIso_DEN_TightIDandIPCut", "TightRelIso_DEN_TightIDandIPCut", "LooseRelTkIso_DEN_HighPtIDandIPCut", "TightRelTkIso_DEN_HighPtIDandIPCut"] :
-    MUON_ISO_ScaleFactors[ iso ] = MuonSF_JSONReader( MUON_ISO_JSON_FileName , "NUM_" + iso )
 
 flashggMuonSystematics = cms.EDProducer('FlashggMuonEffSystematicProducer',
 					src = cms.InputTag("flashggSelectedMuons"),
@@ -101,27 +168,29 @@ flashggMuonSystematics = cms.EDProducer('FlashggMuonEffSystematicProducer',
                                         )
 
 import exceptions
-def SetupMuonScaleFactors( process , id_name , iso_name ):
+def SetupMuonScaleFactors( process , id_file_name, id_lowpt_file_name, iso_file_name, id_name , iso_name , id_ref_tracks, id_lowpt_ref_tracks):
 
+    MUON_ID_ScaleFactors = {}
+    for mu_id in ["Tight", "Medium" , "Loose", "Soft", "HighPt", "MediumPrompt", "TrkHighPt"] :
+        MUON_ID_ScaleFactors[mu_id] = MuonSF_JSONReader( id_file_name ,  "NUM_%sID_DEN_%s"%mu_id%id_ref_tracks , id_lowpt_file_name, "NUM_%sID_DEN_%s"%mu_id%id_lowpt_ref_tracks )
 
-    if os.environ["CMSSW_VERSION"].count("CMSSW_9_4"):
-        if id_name in MUON_ID_ScaleFactors.keys():
-            process.flashggMuonSystematics.SystMethods.append( MUON_ID_ScaleFactors[id_name].GetPSet("Muon" + id_name + "IDWeight") )
-        else :
-            raise NameError("%s id for muon is not valid" % id_name )
+    MUON_ISO_ScaleFactors = {}
+    for iso in ["LooseRelIso_DEN_LooseID", "LooseRelIso_DEN_MediumID", "TightRelIso_DEN_MediumID", "LooseRelIso_DEN_TightIDandIPCut", "TightRelIso_DEN_TightIDandIPCut", "LooseRelTkIso_DEN_HighPtIDandIPCut"] :
+        MUON_ISO_ScaleFactors[ iso ] = MuonSF_JSONReader( iso_file_name , "NUM_" + iso , "", "", 10)
+
+    if id_name in MUON_ID_ScaleFactors.keys():
+        process.flashggMuonSystematics.SystMethods.append( MUON_ID_ScaleFactors[id_name].GetPSet("Muon" + id_name + "IDWeight") )
+    else :
+        raise NameError("%s id for muon is not valid" % id_name )
 
         Extention = ""
-        if id_name in ["Tight" , "HighPt"]:
-            Extention = "andIPCut"
-        iso_full_name = "%sIso_DEN_%sID%s" % (iso_name , id_name , Extention)
-        if iso_full_name in MUON_ISO_ScaleFactors.keys() :
-            process.flashggMuonSystematics.SystMethods.append( MUON_ISO_ScaleFactors[iso_full_name].GetPSet("Muon" + iso_name  + "ISOWeight") )
-        else :
-            raise NameError("%s iso for muon is not valid" % iso_full_name )
+    if id_name in ["Tight" , "HighPt"]:
+        Extention = "andIPCut"
+    iso_full_name = "%sIso_DEN_%sID%s" % (iso_name , id_name , Extention)
+    if iso_full_name in MUON_ISO_ScaleFactors.keys() :
+        process.flashggMuonSystematics.SystMethods.append( MUON_ISO_ScaleFactors[iso_full_name].GetPSet("Muon" + iso_name  + "ISOWeight") )
+    else :
+        raise NameError("%s iso for muon is not valid" % iso_full_name )
 
-    elif os.environ["CMSSW_VERSION"].count("CMSSW_8_0"):
-        import flashgg.Systematics.flashggMuonSystematics2016_cfi as muons2016
-        process.flashggMuonSystematics.SystMethods.append( muons2016.PSetMuonWeight2016 )
-        process.flashggMuonSystematics.SystMethods.append( muons2016.PSetMuonMiniIsoWeight2016 )
         
             
