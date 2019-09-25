e4e3e87d154e29d00218be365eaa2ebea4242052
Prasant Kumar Rout <prasantrout7@gmail.com>
2019-09-25 19:31:33 +0200

    Modified cfg file  for 2018

--- Taggers/test/DiPhotonMVA_Training_byTag_Syst_diphotons_LowMass_2018.py
+++ Taggers/test/DiPhotonMVA_Training_byTag_Syst_diphotons_LowMass_2018.py
@@ -8,36 +8,58 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 import FWCore.ParameterSet.Options as options
 from flashgg.MetaData.samples_utils import SamplesManager
 
+import os
+from flashgg.MetaData.MetaConditionsReader import *
 
-from flashgg.MetaData.JobConfig import customize
-customize.setDefault("metaConditions",'$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2018_RR-17Sep2018_v1.json')
 
 process = cms.Process("DiPhotonMVATrainig")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+
+
+### Global Tag                                                                                                                                 
 from Configuration.AlCa.GlobalTag import GlobalTag
+
+from flashgg.MetaData.JobConfig import customize
+
 process.GlobalTag.globaltag = '102X_upgrade2018_realistic_v15'
+#if customize.processId == "Data":
+#    process.GlobalTag.globaltag = str(customize.metaConditions['globalTags']['data'])
+#else:
+#    process.GlobalTag.globaltag = str(customize.metaConditions['globalTags']['MC'])
+
+
+
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+
 process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring( "root://cms-xrd-global.cern.ch//store/user/spigazzi/flashgg/Era2018_RR-17Sep2018_v2/legacyRun2FullV2/GluGluHToGG_M-125_13TeV_powheg_pythia8/Era2018_RR-17Sep2018_v2-legacyRun2FullV2-v0-RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/190710_090337/0000/myMicroAODOutputFile_13.root"))
 
 
+customize.metaConditions = MetaConditionsReader('$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2018_RR-17Sep2018_v1.json')
 
 from flashgg.Systematics.SystematicsCustomize import *
 
 process.load("flashgg/Taggers/flashggTagSequence_cfi")
 process.load("flashgg/Taggers/flashggPreselectedDiPhotons_cfi")
-process.load("flashgg/Systematics/flashggDiPhotonSystematics2018_cfi")
-#process.load("flashgg/MetaData/data/MetaConditions/Era2018_RR-17Sep2018_v1.json")
+process.load("flashgg/Systematics/flashggDiPhotonSystematics_cfi")
 
-from flashgg.Taggers.flashggDifferentialPhoIdInputsCorrection_cfi import flashggDifferentialPhoIdInputsCorrection
+from flashgg.Taggers.flashggTagSequence_cfi import flashggPrepareTagSequence
+process.flashggTagSequence = flashggPrepareTagSequence(process, customize.metaConditions)
+
+from flashgg.Systematics.flashggDiPhotonSystematics_cfi import flashggDiPhotonSystematics, setupDiPhotonSystematics 
+setupDiPhotonSystematics(process, customize)
+
+from flashgg.Taggers.flashggDifferentialPhoIdInputsCorrection_cfi import flashggDifferentialPhoIdInputsCorrection, setup_flashggDifferentialPhoIdInputsCorrection
 
 useEGMTools(process)
 
-jetSystematicsInputTags = createStandardSystematicsProducers(process)
+jetSystematicsInputTags = createStandardSystematicsProducers(process , customize)
+
 modifyTagSequenceForSystematics(process,jetSystematicsInputTags)
 
 systlabels = [""]
@@ -46,15 +68,7 @@ jetsystlabels = []
 
 
 from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
-##trigger for 2016
-# STD
-#process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring("HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_Mass90_v*") )
-# LOW MASS
-#process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring( "HLT_Diphoton30PV_18PV_R9Id_AND_IsoCaloId_AND_HE_R9Id_DoublePixelVeto_Mass55_v*",
-#                                                                 "HLT_Diphoton30EB_18EB_R9Id_OR_IsoCaloId_AND_HE_R9Id_DoublePixelVeto_Mass55_v*")
-#                                                               )
-##trigger for 2017
-# LOW MASS
+
 process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring("HLT_Diphoton30PV_18PV_R9Id_AND_IsoCaloId_AND_HE_R9Id_PixelVeto_Mass55_v*") )
                                                                 
 
@@ -73,17 +87,12 @@ process.flashggUntagged.Boundaries=cms.vdouble(-2)
 #from flashgg.MetaData.JobConfig import customize
 customize.setDefault("maxEvents",-1)
 #customize.setDefault("processIndex",5)
-#customize.setDefault("targetLumi", 41529.186840973)
+customize.setDefault("targetLumi", 59.74)
 #customize.setDefault("targetLumi",1.e+3)
-customize.setDefault("targetLumi",1.0)
+#customize.setDefault("targetLumi",1.0)
 
 #customize.setDefault("metaConditions",'$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2018_RR-17Sep2018_v1.json')
 
-# OLD PU Target from Vittorio
-#customize.setDefault("puTarget", ' 2.51e+05,1.15e+06,2.47e+06,3.72e+06,5.19e+06,6.79e+06,8.67e+06,2.31e+07,5.89e+07,1.38e+08,3.12e+08,5.71e+08,8.76e+08,1.21e+09,1.56e+09,1.87e+09,2.08e+09,2.19e+09,2.24e+09,2.28e+09,2.29e+09,2.24e+09,2.15e+09,2.03e+09,1.88e+09,1.71e+09,1.54e+09,1.36e+09,1.19e+09,1.01e+09,8.48e+08,6.94e+08,5.57e+08,4.38e+08,3.37e+08,2.53e+08,1.85e+08,1.31e+08,8.96e+07,5.87e+07,3.68e+07,2.2e+07,1.25e+07,6.75e+06,3.46e+06,1.68e+06,7.79e+05,3.44e+05,1.46e+05,6.13e+04,2.68e+04,1.33e+04,8.25e+03,6.30e+03,5.45e+03,4.97e+03,4.59e+03,4.25e+03,3.92e+03,3.58e+03,3.25e+03,2.93e+03,2.62e+03,2.33e+03,2.05e+03,1.79e+03,1.56e+03,1.34e+03,1.14e+03,969,815,680,563,463,378')
-# NEW 2016 PU Target from flashgg/Systematics/test/puTarget_moriond17_69200.txt
-#customize.setDefault("puTarget", ' 2.39e+05,8.38e+05,2.31e+06,3.12e+06,4.48e+06,6.00e+06,7.00e+06,1.29e+07,3.53e+07,7.87e+07,1.77e+08,3.60e+08,6.03e+08,8.77e+08,1.17e+09,1.49e+09,1.76e+09,1.94e+09,2.05e+09,2.10e+09,2.13e+09,2.15e+09,2.13e+09,2.06e+09,1.96e+09,1.84e+09,1.70e+09,1.55e+09,1.40e+09,1.24e+09,1.09e+09,9.37e+08,7.92e+08,6.57e+08,5.34e+08,4.27e+08,3.35e+08,2.58e+08,1.94e+08,1.42e+08,1.01e+08,6.9e+07,4.55e+07,2.88e+07,1.75e+07,1.02e+07,5.64e+06,2.99e+06,1.51e+06,7.32e+05,3.40e+05,1.53e+05,6.74e+04,3.05e+04,1.52e+04,8.98e+03,6.50e+03,5.43e+03,4.89e+03,4.52e+03,4.21e+03,3.91e+03,3.61e+03,3.32e+03,3.03e+03,2.75e+03,2.47e+03,2.21e+03,1.97e+03,1.74e+03,1.52e+03,1.32e+03,1.14e+03,983,839')
-# 2017 PU Target
 customize.setDefault("puTarget", '258796,1.08435e+06,2.01193e+06,3.77861e+06,4.05589e+06,5.87787e+06,6.4516e+06,6.83344e+06,9.25297e+06,2.18232e+07,4.37034e+07,8.26532e+07,1.31504e+08,1.8857e+08,2.66488e+08,3.75273e+08,5.23538e+08,6.97846e+08,8.71195e+08,1.03118e+09,1.17165e+09,1.28523e+09,1.3722e+09,1.43974e+09,1.49771e+09,1.55261e+09,1.59971e+09,1.6293e+09,1.63492e+09,1.61469e+09,1.57114e+09,1.50879e+09,1.43073e+09,1.33931e+09,1.23908e+09,1.13676e+09,1.03828e+09,9.47166e+08,8.657e+08,7.96707e+08,7.43839e+08,7.10458e+08,6.98228e+08,7.06139e+08,7.29876e+08,7.61635e+08,7.90699e+08,8.05265e+08,7.95322e+08,7.55363e+08,6.8604e+08,5.94397e+08,4.9105e+08,3.8729e+08,2.92423e+08,2.12189e+08,1.48643e+08,1.01017e+08,6.69271e+07,4.34303e+07,2.77229e+07,1.74762e+07,1.09192e+07,6.78555e+06,4.20834e+06,2.61362e+06,1.63076e+06,1.02515e+06,650709,417555,270919,177583,117412,78141.3,52235.7,34999.9,23462.5,15711.1,10495.3,6986.61,4630.6,3053.36,2001.73,1303.98,843.625,541.805,345.281,218.261,136.808,85.0054,52.3447,31.9367,19.3024,11.5548,6.84977,4.02065,2.33655,1.34421,0.76548,0.431463')
 
 
@@ -113,12 +122,6 @@ customize.options.register('runOnZ',
 customize.runOnZ = ''
 customize.parse()
 
-
-#FIXME ?
-#if customize.processType == 'data':
-#    process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v8'
-#else:
-#    process.GlobalTag.globaltag = '94X_mc2017_realistic_v12'
 
 
 process.flashggDiPhotonMVA.diphotonMVAweightfile = customize.diphoxml
