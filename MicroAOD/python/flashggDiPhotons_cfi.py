ff4c543122a854877b51ac21e5f32f05efefb980
Julie Malcles <julie.malcles@gmail.com>
2019-10-24 14:28:17 +0200

    fix back dipho cfg

--- MicroAOD/python/flashggDiPhotons_cfi.py
+++ MicroAOD/python/flashggDiPhotons_cfi.py
@@ -12,8 +12,8 @@ flashggDiPhotons = cms.EDProducer('FlashggDiPhotonProducer',
                                   GenParticleTag         = cms.InputTag( "flashggPrunedGenParticles" ),
 
                                   ##Parameters for Legacy Vertex Selector                                                
-                                  vertexIdMVAweightfile   = cms.FileInPath("flashgg/MicroAOD/data/TMVAClassification_BDTVtxId_SL_2016.xml"),
-                                  vertexProbMVAweightfile = cms.FileInPath("flashgg/MicroAOD/data/TMVAClassification_BDTVtxProb_SL_2016.xml"),
+                                  vertexIdMVAweightfile   = cms.FileInPath(""),
+                                  vertexProbMVAweightfile = cms.FileInPath(""),
 
                                   useSingleLeg            = cms.bool(True),
                                   useZerothVertexFromMicro = cms.bool(False),
