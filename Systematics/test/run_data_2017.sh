933586007f2d46a3bb98c9e14cbd7bc636561a38
Edward Scott <e.scott15@ic.ac.uk>
2019-10-24 18:46:14 +0200

    add missing uncert file, commands for running

---
+++ Systematics/test/run_data_2017.sh
@@ -0,0 +1,2 @@
+export X509_USER_PROXY=~/x509up_u72495
+fggRunJobs.py --load data_2017.json -d Data2017_24Oct19 --stage-to /eos/home-e/escott/HggLegacy/WorkspaceTest/Pass0/2017/Data/Raw/ -x cmsRun workspaceStd.py maxEvents=-1 -q testmatch  -n 200 --no-copy-proxy dumpWorkspace=True doStageOne=True
