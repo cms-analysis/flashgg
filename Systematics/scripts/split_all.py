3e47c08fa3a19b086e24b899faf0c8f8c04c3a79
Edward Scott <e.scott15@ic.ac.uk>
2019-11-22 14:17:20 +0100

    update sorters for 1p1

--- Systematics/scripts/split_all.py
+++ Systematics/scripts/split_all.py
@@ -1,9 +1,12 @@
 from os import listdir,popen
 from sys import argv
+stage1p1 = False
 stage1 = False
 inclusive = False
+#FIXME sort out this mess
 if len(argv) > 1:
-    if "tage1" in str(argv[1]): stage1 = True
+    if "1p1" in str(argv[1]): stage1p1 = True
+    elif "tage1" in str(argv[1]): stage1 = True
     elif "ncl" in str(argv[1]): inclusive = True
 
 for fn in listdir("."):
@@ -11,7 +14,8 @@ for fn in listdir("."):
         cmd = "python ../../scripts/sort_by_HTXS.py %s" % fn
         #cmd = "python $CMSSW_BASE/src/flashgg/Systematics/scripts/sort_by_HTXS.py %s" % fn
         #if stage1: cmd = "python ../../scripts/sort_by_HTXSstage1.py %s" % fn
-        if stage1: cmd = "python $CMSSW_BASE/src/flashgg/Systematics/scripts/sort_by_HTXSstage1.py %s" % fn
+        if stage1p1: cmd = "python $CMSSW_BASE/src/flashgg/Systematics/scripts/sort_by_HTXSstage1p1.py %s" % fn
+        elif stage1: cmd = "python $CMSSW_BASE/src/flashgg/Systematics/scripts/sort_by_HTXSstage1.py %s" % fn
         elif inclusive: cmd = "python $CMSSW_BASE/src/flashgg/Systematics/scripts/sort_by_HTXSinclusive.py %s" % fn
         print cmd
         result = popen(cmd).read()
