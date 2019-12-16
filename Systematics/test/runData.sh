1b7299f149c515be5d5e2fc1a802d1a78531e2b4
Thomas Reitenspiess <thomas.reitenspiess@cern.ch>
2019-12-16 15:40:09 +0100

    New options in runData

--- Systematics/test/runData.sh
+++ Systematics/test/runData.sh
@@ -7,12 +7,12 @@ if [[ -z $outDir ]]; then
 fi
 
 mkdir -p /eos/user/t/threiten/Analysis/Differentials/2016ReReco/${outDir}_data_16/
-fggRunJobs.py --load data_jobs_2016.json -d ${outDir}_data_16 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=True acceptance=NONE maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2016ReReco/${outDir}_data_16/ -q tomorrow -n 500 -H --no-copy-proxy &
+fggRunJobs.py --load data_jobs_2016.json -d ${outDir}_data_16 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=True --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True acceptance=NONE maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2016ReReco/${outDir}_data_16/ -q workday -n 500 -H --no-copy-proxy &
 
 mkdir -p /eos/user/t/threiten/Analysis/Differentials/2017ReReco/${outDir}_data_17/
-fggRunJobs.py --load data_jobs_2017.json -d ${outDir}_data_17 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=True acceptance=NONE maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2017ReReco/${outDir}_data_17/ -q tomorrow -n 500 -H --no-copy-proxy &
+fggRunJobs.py --load data_jobs_2017.json -d ${outDir}_data_17 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=True --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True acceptance=NONE maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2017ReReco/${outDir}_data_17/ -q workday -n 500 -H --no-copy-proxy &
 
 mkdir -p /eos/user/t/threiten/Analysis/Differentials/2018ReABCPromptDReco/${outDir}_data_18/
-fggRunJobs.py --load data_jobs_2018.json -d ${outDir}_data_18 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=True acceptance=NONE maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2018ReABCPromptDReco/${outDir}_data_18/ -q tomorrow -n 500 -H --no-copy-proxy &
+fggRunJobs.py --load data_jobs_2018.json -d ${outDir}_data_18 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=True --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True acceptance=NONE maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2018ReABCPromptDReco/${outDir}_data_18/ -q workday -n 500 -H --no-copy-proxy &
 
 wait
