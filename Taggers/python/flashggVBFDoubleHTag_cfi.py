2261730ec467b0e7eccc8f47c2afac359c08ed8e
mukherje <soumya.mukherjee@cern.ch>
2020-04-06 20:12:55 +0200

    R9 variable

--- Taggers/python/flashggVBFDoubleHTag_cfi.py
+++ Taggers/python/flashggVBFDoubleHTag_cfi.py
@@ -82,7 +82,7 @@ flashggVBFDoubleHTag = cms.EDProducer("FlashggVBFDoubleHTagProducer",
 
                                    doMVAFlattening=cms.bool(False),#do transformation of cumulative to make it flat
                                    MVAscaling=cms.double(MVAscalingValue),
-                                   doCategorization=cms.bool(True),#do categorization based on MVA x MX or only fill first tree with all events
+                                   doCategorization=cms.bool(False),#do categorization based on MVA x MX or only fill first tree with all events
                                    #MVAFlatteningFileName=cms.untracked.FileInPath("%s"%MVAFlatteningFileName),#FIXME, this should be optional, is it?
                                    globalVariables=globalVariables,
                                    doReweight = flashggDoubleHReweight.doReweight,
