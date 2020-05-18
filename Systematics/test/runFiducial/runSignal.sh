c6d51f663c9b60b193d7fb526d070e54ee1de262
Thomas Reitenspiess <thomas.reitenspiess@cern.ch>
2020-05-18 11:49:41 +0200

    Adjusted run scripts

--- Systematics/test/runFiducial/runSignal.sh
+++ Systematics/test/runFiducial/runSignal.sh
@@ -7,12 +7,12 @@ if [[ -z $outDir ]]; then
 fi
 
 mkdir -p /eos/user/t/threiten/Analysis/Differentials/2016ReReco/${outDir}_signal_IA_16/
-fggRunJobs.py --load sig_jobs_2016.json -d ${outDir}_signal_IA_16 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=False --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True puTarget=`cat lumi/PU2016.txt`  maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2016ReReco/${outDir}_signal_IA_16/ -q workday -n 100 -H --no-copy-proxy &
+fggRunJobs.py --load sig_jobs_2016.json -d ${outDir}_signal_IA_16 -x cmsRun ../fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=False --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True puTarget=`cat lumi/PU2016.txt`  maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2016ReReco/${outDir}_signal_IA_16/ -q workday -n 100 -H --no-copy-proxy &
 
 mkdir -p /eos/user/t/threiten/Analysis/Differentials/2017ReReco/${outDir}_signal_IA_17/
-fggRunJobs.py --load sig_jobs_2017.json -d ${outDir}_signal_IA_17 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=False --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True puTarget=`cat lumi/PU2017.txt` maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2017ReReco/${outDir}_signal_IA_17/ -q workday -n 100 -H --no-copy-proxy &
+fggRunJobs.py --load sig_jobs_2017.json -d ${outDir}_signal_IA_17 -x cmsRun ../fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=False --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True puTarget=`cat lumi/PU2017.txt` maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2017ReReco/${outDir}_signal_IA_17/ -q workday -n 100 -H --no-copy-proxy &
 
 mkdir -p /eos/user/t/threiten/Analysis/Differentials/2018ReABCPromptDReco/${outDir}_signal_IA_18/
-fggRunJobs.py --load sig_jobs_2018.json -d ${outDir}_signal_IA_18 -x cmsRun fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=False --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True puTarget=`cat lumi/PU2018.txt` maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2018ReABCPromptDReco/${outDir}_signal_IA_18/ -q workday -n 100 -H --no-copy-proxy &
+fggRunJobs.py --load sig_jobs_2018.json -d ${outDir}_signal_IA_18 -x cmsRun ../fiducialStd.py metaDataSrc=flashgg useAAA=True dumpWorkspace=False dumpTrees=True doSystematics=False doJets=True doBJetsAndMET=False --nCondorCpu=2 --make-light-tarball copyInputMicroAOD=True puTarget=`cat lumi/PU2018.txt` maxEvents=-1 --stage-to=/eos/user/t/threiten/Analysis/Differentials/2018ReABCPromptDReco/${outDir}_signal_IA_18/ -q workday -n 100 -H --no-copy-proxy &
 
 wait
