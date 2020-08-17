99e5eb5e80e8314b4c32510df81e1092029db4e7
chu <chuw@cern.ch>
2020-08-17 05:30:55 +0200

    merge

--- flashgg/cmdssig.sh
+++
@@ -1,1 +0,0 @@
-sh HHWWgg_Run_Jobs.sh --labelName Test_Cut_Signal_new_Dipho_m --nEvents all --json Taggers/test/HHWWgg_Full/HHWWgg_FL_v2.json -g -s -w 
--- flashgg/cmdssig_withTau_Non.sh
+++
@@ -1,1 +0,0 @@
-sh HHWWgg_Run_Jobs.sh --labelName old_noHadronic_withTau_SM --nEvents all --json Taggers/test/HHWWgg_Full/Non_res_old.json -g -c -t 
