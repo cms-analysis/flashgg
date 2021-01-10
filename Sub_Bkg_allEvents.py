97a99c3b72d7deedf0478a7d193894f49502fd15
Abe Tishelman-Charny <abe2323@yahoo.com>
2021-01-10 17:09:10 +0100

    Tiny Script Update

--- Sub_Bkg_allEvents.py
+++ Sub_Bkg_allEvents.py
@@ -13,7 +13,7 @@ if (not os.path.isdir(outDirec)):
     exit(1)
 
 # outputDirectory = "/eos/user/a/atishelm/ntuples/HHWWgg_flashgg/January_2021_Production/2017/Backgrounds_Lowevents/Flashgg_bkg_short_lowEvents/"
-command = "fggRunJobs.py --load Taggers/test/HHWWgg/January_2021_Production/2017/Backgrounds/Flashgg_bkg.json -D -P -n 500 -d Flashgg_2017_bkg_AllEvents --stage-to=%s -x cmsRun Systematics/test/workspaceStd.py maxEvents=-1 -q tomorrow --no-use-tarball --no-copy-proxy metaConditions=$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2017_RR-31Mar2018_v1-HHWWgg.json doHHWWggTag=1 HHWWggTagsOnly=1 doSystematics=0 dumpTrees=1 dumpWorkspace=0 doHHWWggTagCutFlow=1 saveHHWWggFinalStateVars=1 copyInputMicroAOD=1"%(outDirec)
+command = "fggRunJobs.py --load Taggers/test/HHWWgg/January_2021_Production/2017/Backgrounds/Flashgg_bkg.json -D -P -n 500 -d Flashgg_2017_bkg_AllEvents --stage-to=%s/ -x cmsRun Systematics/test/workspaceStd.py maxEvents=-1 -q tomorrow --no-use-tarball --no-copy-proxy metaConditions=$CMSSW_BASE/src/flashgg/MetaData/data/MetaConditions/Era2017_RR-31Mar2018_v1-HHWWgg.json doHHWWggTag=1 HHWWggTagsOnly=1 doSystematics=0 dumpTrees=1 dumpWorkspace=0 doHHWWggTagCutFlow=1 saveHHWWggFinalStateVars=1 copyInputMicroAOD=1"%(outDirec)
 print"Submit command:"
 print "====================="
 print command 
