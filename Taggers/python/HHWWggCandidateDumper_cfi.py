7dd0150937e6a9949c195ae5bf66ef74b986860c
Abe Tishelman-Charny <abe2323@yahoo.com>
2021-02-04 12:43:56 +0100

    Removed Unused HHWWgg Candidate Dumper Files

--- Taggers/python/HHWWggCandidateDumper_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from flashgg.Taggers.HHWWggCandidateDumpConfig_cff import HHWWggCandidateDumpConfig
-
-HHWWggCandidateDumper = cms.EDAnalyzer('CutBasedHHWWggCandidateDumper',
-                                    **HHWWggCandidateDumpConfig.parameters_() 
-                                    )
