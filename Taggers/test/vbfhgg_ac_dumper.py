0df6d1c9a28fac1d4ce37b6b470d03e85f4a771f
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2021-06-04 16:54:43 +0200

    update to make 2017 trees

--- Taggers/test/vbfhgg_ac_dumper.py
+++ Taggers/test/vbfhgg_ac_dumper.py
@@ -15,7 +15,7 @@ process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 process.maxEvents   = cms.untracked.PSet( input  = cms.untracked.int32( 10 ) )
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1 )
 
 systlabels    = [""]
 phosystlabels = []
